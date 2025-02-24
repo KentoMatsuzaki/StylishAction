e8e7d8500e64e161cc93daa4f31fbb52c64d60e0
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-02-24 23:13:05 +0900

    [add] アクションノードのクラス

---
+++ Assets/Scripts/Enemy/Nodes/ActionNode.cs
@@ -0,0 +1,25 @@
+using Enum;
+using System;
+
+namespace Enemy.Nodes
+{
+    /// <summary>アクションノード</summary>
+    public class ActionNode : BaseNode
+    {
+        /// <summary>アクション</summary>
+        readonly Func<EnemyEnum.NodeStatus> _action;
+
+        /// <summary>コンストラクター</summary>
+        public ActionNode(Func<EnemyEnum.NodeStatus> action)
+        {
+            _action = action;
+        }
+
+        /// <summary>ノードの評価結果を返す</summary>
+        /// <returns>Success = 成功, Failure = 失敗, Running = 実行中</returns>
+        public override EnemyEnum.NodeStatus Execute()
+        {
+            return _action();
+        }
+    }
+}
