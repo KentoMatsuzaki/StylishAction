d76bf1d6c9bf1d01307c8ab1cd868966395281eb
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-02-26 22:42:13 +0900

    [add] 敵に関する定数をまとめたクラス

---
+++ Assets/Scripts/Const/EnemyConst.cs
@@ -0,0 +1,18 @@
+namespace Const
+{
+    /// <summary>敵に関する定数をまとめたクラス</summary>
+    public static class EnemyConst
+    {
+        //-------------------------------------------------------------------------------
+        // アニメーション（トリガー）
+        //-------------------------------------------------------------------------------
+        
+        public const string Skill1Trigger = "Skill1Trigger"; // スキル1のトリガー名
+        public const string Skill2Trigger = "Skill2Trigger"; // スキル2のトリガー名
+        public const string Skill3Trigger = "Skill3Trigger"; // スキル3のトリガー名
+        public const string Skill4Trigger = "Skill4Trigger"; // スキル4のトリガー名
+        public const string Skill5Trigger = "Skill5Trigger"; // スキル5のトリガー名
+        public const string Skill6Trigger = "Skill6Trigger"; // スキル6のトリガー名
+        public const string Skill7Trigger = "Skill7Trigger"; // スキル7のトリガー名
+    }
+}
