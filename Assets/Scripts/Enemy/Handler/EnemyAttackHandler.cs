467864376bafc60a17ad71ba0ff56857c19ea957
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-02-27 20:05:20 +0900

    [add] 敵の攻撃を制御するクラス

---
+++ Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+
+namespace Enemy.Handler
+{
+    /// <summary>敵の攻撃を制御するクラス</summary>
+    public class EnemyAttackHandler : MonoBehaviour
+    {
+        
+    }
+}
