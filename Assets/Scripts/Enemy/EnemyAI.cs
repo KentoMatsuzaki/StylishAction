c84817c7b009f082db531cd384176427e348e0ef
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-02-27 14:28:32 +0900

    [add] AIを実行するメソッド

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,22 +1,50 @@
+using System.Threading;
+using Enemy.AsyncNodes;
+using Cysharp.Threading.Tasks;
+using Enemy.Handler;
+using Enum;
+using UnityEngine;
+
 namespace Enemy
 {
     /// <summary>敵のAIを制御するクラス</summary>
-    public class EnemyAI
+    public class EnemyAI : MonoBehaviour
     {
+        private CancellationTokenSource _cts = new();
+        private BaseAsyncNode _rootNode;
+        private EnemyAnimationHandler _animationHandler;
+
         private void Start()
         {
+            _animationHandler = GetComponent<EnemyAnimationHandler>();
             
+            var attack = new AsyncActionNode(async (token) =>
+            {
+                await _animationHandler.WaitForAnimationEnd(token);
+                Debug.Log("Test");
+                return EnemyEnum.NodeStatus.Success;
+            });
+
+            var sequence = new AsyncSequenceNode();
+            sequence.AddNode(attack);
+            _rootNode = sequence;
+
+            RunAI(_cts.Token).Forget();
         }
 
-        private void Update()
+        private async UniTask RunAI(CancellationToken token)
         {
-            
+            while (!token.IsCancellationRequested)
+            {
+                var result = await _rootNode.ExecuteAsync(token);
+                await UniTask.Yield(PlayerLoopTiming.Update, token);
+            }
         }
-        
+
         //-------------------------------------------------------------------------------
         // 攻撃に関する処理
         //-------------------------------------------------------------------------------
-        
-        
+
+
     }
 }
