dacc9b66fadc2db5ae6bb0f685d9e815581fd84d
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-02-28 16:04:45 +0900

    [del] 不要なアニメーショントリガー名を削除

--- Assets/Scripts/Const/EnemyConst.cs
+++ Assets/Scripts/Const/EnemyConst.cs
@@ -6,13 +6,5 @@ namespace Const
         //-------------------------------------------------------------------------------
         // アニメーション（トリガー）
         //-------------------------------------------------------------------------------
-        
-        public const string Skill1Trigger = "Skill1Trigger"; // スキル1のトリガー名
-        public const string Skill2Trigger = "Skill2Trigger"; // スキル2のトリガー名
-        public const string Skill3Trigger = "Skill3Trigger"; // スキル3のトリガー名
-        public const string Skill4Trigger = "Skill4Trigger"; // スキル4のトリガー名
-        public const string Skill5Trigger = "Skill5Trigger"; // スキル5のトリガー名
-        public const string Skill6Trigger = "Skill6Trigger"; // スキル6のトリガー名
-        public const string Skill7Trigger = "Skill7Trigger"; // スキル7のトリガー名
     }
 }
