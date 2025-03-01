093f9253977b59e7c5b1ff9f2e6a866d71550bfe
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-01 21:39:04 +0900

    [add] 攻撃コライダーの処理

--- Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
+++ Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
-using System.Threading;
-using Cysharp.Threading.Tasks;
+using Const;
+using Player;
 
 namespace Enemy.Handler
 {
     /// <summary>敵の攻撃を制御するクラス</summary>
     public class EnemyAttackHandler : MonoBehaviour
     {
+        /// <summary>敵の制御クラス</summary>
+        private EnemyController _controller;
         
+        /// <summary>攻撃の当たり判定</summary>
+        private Collider _collider;
+
+        private void Start()
+        {
+            _controller = GetComponentInParent<EnemyController>();
+            _collider = GetComponent<Collider>();
+            _collider.enabled = false;
+            _collider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(InGameConst.PlayerTagName))
+            {
+                var player = other.GetComponent<PlayerController>();
+                _controller.CurrentBehaviourTree.OnHitPlayer();
+            }
+        }
+        
+        /// <summary>攻撃の当たり判定を有効にする</summary>
+        public void EnableAttackCollider() => _collider.enabled = true;
+        
+        /// <summary>攻撃の当たり判定を無効にする</summary>
+        public void DisableAttackCollider() => _collider.enabled = false;
     }
 }
