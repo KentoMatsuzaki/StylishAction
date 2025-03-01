b4943bf9b2b4e36a998ad202f1d06f7f542eef54
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-01 17:16:38 +0900

    [add] 敵を制御するクラス

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,7 +1,29 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Enemy.AI;
+using UnityEngine;
+
 namespace Enemy
 {
-    public class EnemyController
+    /// <summary>敵を制御するクラス</summary>
+    public class EnemyController : MonoBehaviour
     {
+        [SerializeField] private Phase1AI phase1AI;
         
+        private CancellationTokenSource _cts;
+        private IEnemyAI _bt;
+
+        private void Start()
+        {
+
+        }
+
+        private void BeginBehaviourTree()
+        {
+            // キャンセルトークンを作成する
+            _cts = new CancellationTokenSource();
+            // ビヘイビアツリーを実行する
+            _bt.ExecuteBehaviourTree(_cts.Token).Forget();
+        }
     }
 }
