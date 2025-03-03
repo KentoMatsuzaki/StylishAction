9c1f6a17569adea28927cb9327f795aa32e5f364
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-03 19:34:36 +0900

    [update] 敵の攻撃を制御するクラスを敵の攻撃クラスに変更

---
+++ Assets/Scripts/Enemy/Handler/EnemyAttacker.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Const;
+using Player;
+
+namespace Enemy.Handler
+{
+    /// <summary>敵の攻撃クラス</summary>
+    public class EnemyAttacker : MonoBehaviour
+    {
+        /// <summary>敵の制御クラス</summary>
+        private EnemyController _controller;
+        
+        /// <summary>攻撃コライダー</summary>
+        private Collider _collider;
+
+        private void Start()
+        {
+            _controller = GetComponentInParent<EnemyController>();
+            _collider = GetComponent<Collider>();
+            _collider.enabled = false;
+            _collider.isTrigger = true;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(InGameConst.PlayerTagName))
+            {
+                var player = other.GetComponent<PlayerController>();
+                //_controller.OnHitPlayer(player);
+            }
+        }
+        
+        /// <summary>攻撃コライダーを有効にする</summary>
+        public void EnableAttackCollider() => _collider.enabled = true;
+        
+        /// <summary>攻撃コライダーを無効にする</summary>
+        public void DisableAttackCollider() => _collider.enabled = false;
+    }
+}
