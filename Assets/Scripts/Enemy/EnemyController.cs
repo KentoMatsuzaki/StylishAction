a5c4579789f76db6c7205421eb59722ade4ec3a4
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-03 19:04:44 +0900

    [update] パリィ処理をAI側に移植

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,9 +1,6 @@
 using System.Threading;
-using Const;
-using Cysharp.Threading.Tasks;
 using Enemy.AI;
 using Enemy.Handler;
-using Player;
 using UnityEngine;
 
 namespace Enemy
@@ -11,70 +8,21 @@ namespace Enemy
     /// <summary>敵を制御するクラス</summary>
     public class EnemyController : MonoBehaviour
     {
-        private Phase1AI _phase1AI;
+        private Phase1AIBase _phase1AIBase;
         
         private CancellationTokenSource _cts;
-        private IEnemyAI _bt;
+        private EnemyAIBase _bt;
         
+        /// <summary></summary>
         private EnemyAnimationHandler _animationHandler;
-
-        [Header("スキル1の攻撃クラス"), SerializeField] private EnemyAttackHandler skill1AttackHandler;
-        [Header("スキル2の攻撃クラス"), SerializeField] private EnemyAttackHandler skill2AttackHandler;
 
         private void Start()
         {
             _animationHandler = GetComponent<EnemyAnimationHandler>();
-            _phase1AI = new Phase1AI(_animationHandler, skill1AttackHandler, skill2AttackHandler);
+            _phase1AIBase = new Phase1AIBase(_animationHandler);
         }
 
-        private void BeginBehaviourTree()
-        {
-            // キャンセルトークンを作成する
-            _cts = new CancellationTokenSource();
-            // ビヘイビアツリーを実行する
-            _bt.ExecuteBehaviourTree(_cts.Token).Forget();
-        }
-        
-        //-------------------------------------------------------------------------------
-        // パリィに関する処理
-        //-------------------------------------------------------------------------------
-
-        /// <summary>プレイヤーに攻撃が命中した時の処理</summary>
-        public void OnHitPlayer(PlayerController player)
-        {
-            // プレイヤーがパリィ状態である場合
-            OnParried();
-            // プレイヤーがパリィ状態でない場合
-            // player.TakeDamage
-        }
-
-        /// <summary>プレイヤーにパリィされた時の処理</summary>
-        private void OnParried()
-        {
-            // ビヘイビアツリーをキャンセルする
-            _cts?.Cancel();
-            // スタン処理を呼ぶ
-            Stun();
-        }
-
-        /// <summary>敵のスタン時の処理</summary>
-        private async void Stun()
-        {
-            // アニメーションを再生する
-            _animationHandler.PlayAnimation(InGameConst.EnemyStunAnimation);
-            // スタンしている間は待機する
-            await UniTask.Delay(2000);
-            // ビヘイビアツリーを再開する
-            BeginBehaviourTree();
-        }
-        
-        //-------------------------------------------------------------------------------
-        // 攻撃コライダーに関する処理
-        //-------------------------------------------------------------------------------
-
-        public void EnableSkill1AttackCollider() => skill1AttackHandler.EnableAttackCollider();
-        public void DisableSkill1AttackCollider() => skill1AttackHandler.DisableAttackCollider();
-        public void EnableSkill2AttackCollider() => skill2AttackHandler.EnableAttackCollider();
-        public void DisableSkill2AttackCollider() => skill2AttackHandler.DisableAttackCollider();
+        /// <summary>ビヘイビアツリーを開始する</summary>
+        private void BeginBehaviourTree() => _bt.BeginBehaviourTree();
     }
 }
