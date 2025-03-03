75bf9fee5d80ab153d1c32ed74531aceeb6bd012
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-03 19:34:58 +0900

    [add] 敵の攻撃を制御するクラスを追加し、攻撃コライダーの処理を追加

--- Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
+++ Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
@@ -1,7 +1,42 @@
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace Enemy.Handler
 {
-    public class EnemyAttackHandler
+    /// <summary>敵の攻撃を制御するクラス</summary>
+    public class EnemyAttackHandler : MonoBehaviour
     {
-        
+        [Header("敵の攻撃クラスのリスト"), SerializeField] private List<EnemyAttacker> enemyAttackers = new();
+
+        /// <summary>攻撃コライダーを有効化する</summary>
+        /// <param name="skillNumber">対応するスキルの番号</param>
+        public void EnableAttackCollider(int skillNumber)
+        {
+            // スキルの番号が正しくない場合は処理を抜ける
+            if (!IsValidSkillNumber(skillNumber)) return;
+            // 攻撃コライダーを有効化する
+            enemyAttackers[skillNumber - 1].EnableAttackCollider();
+        }
+
+        /// <summary>攻撃コライダーを無効化する</summary>
+        /// <param name="skillNumber">対応するスキルの番号</param>
+        public void DisableAttackCollider(int skillNumber)
+        {
+            // スキルの番号が正しくない場合は処理を抜ける
+            if (!IsValidSkillNumber(skillNumber)) return;
+            // 攻撃コライダーを無効化する
+            enemyAttackers[skillNumber - 1].DisableAttackCollider();
+        }
+
+        /// <summary>スキルの番号が正常であるか</summary>
+        private bool IsValidSkillNumber(int skillNumber)
+        {
+            if (skillNumber < 1 || skillNumber > enemyAttackers.Count)
+            {
+                Debug.LogWarning($"Invalid Skill Number : {skillNumber}");
+                    return false;
+            }
+            return true;
+        }
     }
 }
