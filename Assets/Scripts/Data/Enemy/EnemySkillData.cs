19e587ea78ecae76a1b3305ac046818abbc0ee94
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-06 18:24:43 +0900

    [add] 敵のスキルデータを保持するスクリプタブルオブジェクトを追加

---
+++ Assets/Scripts/Data/Enemy/EnemySkillData.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Data.Enemy
+{
+    /// <summary>敵のスキル情報を保持するスクリプタブルオブジェクト</summary>
+    [CreateAssetMenu(fileName = "EnemySkillData", menuName = "ScriptableObjects/CreateEnemySkillAsset")]
+    public class EnemySkillData : ScriptableObject
+    {
+        /// <summary>スキルの固有番号</summary>
+        public int skillNumber;
+
+        /// <summary>スキルが与えるダメージ量</summary>
+        public float damageAmount;
+
+        /// <summary>スキルの攻撃範囲</summary>
+        public float attackRange;
+    }
+}
