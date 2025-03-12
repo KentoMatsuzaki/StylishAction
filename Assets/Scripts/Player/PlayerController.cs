5e5c2f6afdb423128f4a04b8e9ce3ab8dde78374
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-12 21:43:05 +0900

    [del] テスト用のコードを削除

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,29 +3,9 @@ using UnityEngine;
 
 namespace Player
 {
+    /// <summary>プレイヤーを制御するクラス</summary>
     public class PlayerController : MonoBehaviour
     {
-        private void Update()
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Translate(Vector3.left * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(Vector3.right * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                transform.Translate(Vector3.forward * Time.deltaTime);
-            }
-
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(Vector3.back * Time.deltaTime);
-            }
-        }
+        
     }
 }
