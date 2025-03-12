58b12a8cc32228364da82ca0e05d1dcc2663f94c
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-12 19:39:10 +0900

    [add] commit漏れ

--- Assets/Scripts/Enemy/AsyncNode/BaseAsyncNode.cs
+++ Assets/Scripts/Enemy/AsyncNode/BaseAsyncNode.cs
@@ -5,7 +5,7 @@ using Enum;
 namespace Enemy.AsyncNode
 {
     /// <summary>非同期ノードの基底クラス</summary>
-    public abstract class BaseAsyncNode
+    public abstract class BaseAsyncNode 
     {
         /// <summary>ノードの評価結果を返す</summary>
         /// <returns>Success = 成功, Failure = 失敗, Running = 実行中</returns>
