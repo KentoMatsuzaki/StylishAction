7956898dfd6bab7d77287eccee7eeeff29e3379f
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-13 18:09:48 +0900

    [add] プレイヤーに関する列挙型をまとめたクラス

---
+++ Assets/Scripts/Enum/Player/PlayerEnum.cs
@@ -0,0 +1,17 @@
+namespace Enum.Player
+{
+    /// <summary>プレイヤーに関する列挙型をまとめたクラス</summary>
+    public static class PlayerEnum 
+    {
+        /// <summary>プレイヤーの状態</summary>
+        public enum PlayerState
+        {
+            Idle,   // 静止
+            Move,   // 移動
+            Attack, // 攻撃
+            Parry,  // パリィ
+            Stun,   // スタン
+            Dead    // 死亡
+        }
+    }
+}
