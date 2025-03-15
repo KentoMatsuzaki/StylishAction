dc3a079d2e214880ae61fa463c027efdf7da7df2
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-15 18:19:53 +0900

    [add] フェーズごとの体力・ノックバックの速度を追加

--- Assets/Scripts/Data/Enemy/EnemyStatusData.cs
+++ Assets/Scripts/Data/Enemy/EnemyStatusData.cs
@@ -6,10 +6,22 @@ namespace Data.Enemy
     [CreateAssetMenu(fileName = "EnemyStatusData", menuName = "ScriptableObjects/CreateEnemyStatusAsset")]
     public class EnemyStatusData : ScriptableObject
     {
+        /// <summary>フェーズ1の最大体力</summary>
+        public float phase1MaxHp;
+        
+        /// <summary>フェーズ2の最大体力</summary>
+        public float phase2MaxHp;
+        
+        /// <summary>フェーズ3の最大体力</summary>
+        public float phase3MaxHp;
+        
         /// <summary>移動速度</summary>
         public float moveSpeed;
         
         /// <summary>回転速度</summary>
         public float rotateSpeed;
+
+        /// <summary>ノックバック速度</summary>
+        public float knockbackSpeed;
     }
 }
