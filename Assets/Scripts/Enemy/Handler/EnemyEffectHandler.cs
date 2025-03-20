f2a03d929b7cc73dc41c7f93bf104202730bfa25
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-20 23:52:16 +0900

    [update] 敵のエフェクトの制御方法を変更

--- Assets/Scripts/Enemy/Handler/EnemyEffectHandler.cs
+++ Assets/Scripts/Enemy/Handler/EnemyEffectHandler.cs
@@ -1,19 +1,23 @@
-using UnityEngine;
-using System.Collections.Generic;
 using Effect;
+using Enum.Enemy;
+using UnityEngine;
 
 namespace Enemy.Handler
 {
     /// <summary>敵のエフェクトを制御するクラス</summary>
     public class EnemyEffectHandler : MonoBehaviour
     {
-        [Header("攻撃エフェクト"), SerializeField] private List<GameObject> attackEffects;
-
         /// <summary>攻撃エフェクトを有効化する</summary>
-        /// <param name="skillNumber">スキルの番号(1~)</param>
-        public void ActivateAttackEffect(int skillNumber)
+        public void PlayAttackEffect(string skillName)
         {
-            attackEffects[skillNumber - 2].SetActive(true);
+            if (System.Enum.TryParse(skillName, out EnemyEnum.EnemySkillType type))
+            {
+                EffectManager.Instance.PlayEnemyAttackEffect(type);
+            }
+            else
+            {
+                Debug.LogWarning($"Invalid Skill Name : {skillName}");
+            }
         }
     }
 }
