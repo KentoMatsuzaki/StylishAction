6358cb0aaca552b5cdf22723629a971b8d89f97e
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-21 07:54:20 +0900

    [add] スキルによる敵の攻撃の派生クラスを追加

---
+++ Assets/Scripts/Common/Attacker/EnemySkillAttacker.cs
@@ -0,0 +1,29 @@
+using Const;
+using Enemy;
+using Player;
+using UnityEngine;
+
+namespace Common.Attacker
+{
+    /// <summary>スキルによる敵の攻撃の派生クラス</summary>
+    public class EnemySKillAttacker : SkillAttackerBase
+    {
+        /// <summary>敵の制御クラス</summary>
+        private EnemyController _controller;
+        protected override void Awake()
+        {
+            base.Awake();
+            _controller = GetComponentInParent<EnemyController>();
+        }
+        protected override void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(InGameConst.PlayerTag))
+            {
+                // プレイヤーの制御クラスを取得する
+                var playerController = other.GetComponent<PlayerController>();
+                // 制御クラスの攻撃命中処理を呼ぶ
+                _controller.Bt.OnHitPlayer(playerController);
+            }
+        }
+    }
+}
