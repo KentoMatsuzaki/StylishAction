da2f8531e3f418fe2053cc0cfbade7902295e9f2
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-20 23:52:00 +0900

    [add] 敵の攻撃の制御方法を変更

--- Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
+++ Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
@@ -7,36 +7,18 @@ namespace Enemy.Handler
     /// <summary>敵の攻撃を制御するクラス</summary>
     public class EnemyAttackHandler : MonoBehaviour
     {
-        [Header("敵の攻撃クラスのリスト"), SerializeField] private List<EnemyAttacker> attackerList = new();
-
-        /// <summary>攻撃コライダーを有効化する</summary>
-        /// <param name="skillNumber">対応するスキルの番号</param>
-        public void EnableAttackCollider(int skillNumber)
+        [Header("鎌の攻撃クラス"), SerializeField] private EnemyAttacker scytheAttacker;
+        
+        /// <summary>鎌の攻撃コライダーを有効化する</summary>
+        public void EnableScytheCollider()
         {
-            // スキルの番号が正しくない場合は処理を抜ける
-            if (!IsValidSkillNumber(skillNumber)) return;
-            // 攻撃コライダーを有効化する
-            attackerList[skillNumber - 1].EnableAttackCollider();
+            scytheAttacker.EnableAttackCollider();
         }
 
-        /// <summary>攻撃コライダーを無効化する</summary>
-        /// <param name="skillNumber">対応するスキルの番号</param>
-        public void DisableAttackCollider(int skillNumber)
+        /// <summary>鎌の攻撃コライダーを無効化する</summary>
+        public void DisableScytheCollider()
         {
-            // スキルの番号が正しくない場合は処理を抜ける
-            if (!IsValidSkillNumber(skillNumber)) return;
-            // 攻撃コライダーを無効化する
-            attackerList[skillNumber - 1].DisableAttackCollider();
-        }
-
-        /// <summary>スキルの番号が正常であるか</summary>
-        private bool IsValidSkillNumber(int skillNumber)
-        {
-            if (skillNumber < 1 || skillNumber > attackerList.Count)
-            {
-                Debug.LogWarning($"Invalid Skill Number : {skillNumber}"); return false;
-            }
-            return true;
+            scytheAttacker.DisableAttackCollider();
         }
     }
 }
