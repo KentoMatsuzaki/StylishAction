deca76bf40676201f13760ddc180e11460416a6f
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-21 23:40:05 +0900

    [update] エフェクトの管理方法を大幅に修正

--- Assets/Scripts/Effect/EffectManager.cs
+++ Assets/Scripts/Effect/EffectManager.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using Effect.Enemy;
-using Effect.Player;
 using Enum.Enemy;
 using Enum.Player;
 using UnityEngine;
@@ -11,33 +9,69 @@ namespace Effect
     /// <summary>エフェクトを管理するクラス</summary>
     public class EffectManager : MonoBehaviour
     {
-        public static EffectManager Instance;
-
-        [Header("プレイヤーの攻撃エフェクト"), SerializeField]
+        [Header("プレイヤーの攻撃エフェクトのリスト"), SerializeField]
         private List<PlayerParticleController> playerAttackEffectList;
 
-        [Header("敵の攻撃エフェクト"), SerializeField] 
+        [Header("敵の攻撃エフェクトのリスト"), SerializeField] 
         private List<EnemyParticleController> enemyAttackEffectList;
+
+        /// <summary>プレイヤーの攻撃エフェクトの辞書</summary>
+        private Dictionary<PlayerEnum.PlayerAttackType, PlayerParticleController> _playerAttackEffectDic;
+
+        /// <summary>敵の攻撃エフェクトの辞書</summary>
+        private Dictionary<EnemyEnum.EnemyAttackType, EnemyParticleController> _enemyAttackEffectDic;
+        
+        public static EffectManager Instance;
 
         private void Awake()
         {
             if (Instance == null) Instance = this;
-            else Destroy(this);
+            else Destroy(gameObject);
+            
+            _playerAttackEffectDic = playerAttackEffectList.ToDictionary(e => e.type, e => e);
+            _enemyAttackEffectDic = enemyAttackEffectList.ToDictionary(e => e.type, e => e);
         }
 
         /// <summary>プレイヤーの攻撃エフェクトを有効化する</summary>
         /// <param name="type">攻撃の種類</param>
-        public void PlayPlayerAttackEffect(PlayerEnum.PlayerAttackType type)
+        public void ActivatePlayerAttackEffect(PlayerEnum.PlayerAttackType type)
         {
-            playerAttackEffectList.FirstOrDefault(effect => effect.type == type)?.gameObject.SetActive(true);
+            if (_playerAttackEffectDic.TryGetValue(type, out var effect))
+            {
+                if (effect.gameObject.activeSelf)
+                {
+                    effect.RestartParticles();
+                }
+                else
+                {
+                    effect.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Attack Effect Not Found : {type}");
+            }
         }
 
         /// <summary>敵の攻撃エフェクトを有効化する</summary>
-        /// <param name="type">スキルの種類</param>
-        public void PlayEnemyAttackEffect(EnemyEnum.EnemySkillType type)
+        /// <param name="type">攻撃の種類</param>
+        public void ActivateEnemyAttackEffect(EnemyEnum.EnemyAttackType type)
         {
-            enemyAttackEffectList.FirstOrDefault(effect => effect.type == type)?.gameObject.SetActive(true);
+            if (_enemyAttackEffectDic.TryGetValue(type, out var effect))
+            {
+                if (effect.gameObject.activeSelf)
+                {
+                    effect.RestartParticles();
+                }
+                else
+                {
+                    effect.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"Attack Effect Not Found : {type}");
+            }
         }
     }
-
 }
