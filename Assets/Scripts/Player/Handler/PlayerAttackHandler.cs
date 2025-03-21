c32de4f2957e076af41bec57ea160b5286490911
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-21 23:40:39 +0900

    [update] プレイヤーの攻撃コライダーの管理方法を変更

--- Assets/Scripts/Player/Handler/PlayerAttackHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerAttackHandler.cs
@@ -1,41 +1,42 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Common.Attacker;
 
 namespace Player.Handler
 {
     /// <summary>プレイヤーの攻撃を制御するクラス</summary>
     public class PlayerAttackHandler : MonoBehaviour
     {
-        [Header("攻撃クラスのリスト"), SerializeField] private List<PlayerAttacker> attackerList = new();
+        [Header("武器による攻撃の派生クラスのリスト"), SerializeField] 
+        private List<PlayerWeaponAttacker> weaponAttackerList;
 
-        /// <summary>攻撃コライダーを有効化する</summary>
-        /// <param name="attackNumber">1から始まる、有効化する攻撃の番号</param>
-        public void EnableAttackCollider(int attackNumber)
+        /// <summary>コライダーを有効化する</summary>
+        /// <param name="attackNumber">攻撃の番号(1~)</param>
+        public void EnableCollider(int attackNumber)
         {
             // 攻撃の番号が正常でない場合は処理を抜ける
             if (!IsValidAttackNumber(attackNumber)) return;
             // 攻撃コライダーを有効化する
-            attackerList[attackNumber - 1].EnableAttackCollider();
+            weaponAttackerList[attackNumber - 1].EnableCollider();
         }
 
-        /// <summary>攻撃コライダーを無効化する</summary>
-        /// <param name="attackNumber">1から始まる、無効化する攻撃の番号</param>
-        public void DisableAttackCollider(int attackNumber)
+        /// <summary>コライダーを無効化する</summary>
+        /// <param name="attackNumber">攻撃の番号(1~)</param>
+        public void DisableCollider(int attackNumber)
         {
             // 攻撃の番号が正常出ない場合は処理を抜ける
             if (!IsValidAttackNumber(attackNumber)) return;
             // 攻撃コライダーを無効化する
-            attackerList[attackNumber - 1].DisableAttackCollider();
+            weaponAttackerList[attackNumber - 1].DisableCollider();
         }
 
-        /// <summary>攻撃の番号が正常であるか判定する</summary>
+        /// <summary>正しい攻撃の番号であるか</summary>
         private bool IsValidAttackNumber(int attackNumber)
         {
-            if (attackNumber < 1 || attackNumber > attackerList.Count)
+            if (attackNumber < 1 || attackNumber > weaponAttackerList.Count)
             {
                 Debug.LogWarning($"Invalid Attack Number : {attackNumber}"); return false;
             }
-
             return true;
         }
     }
