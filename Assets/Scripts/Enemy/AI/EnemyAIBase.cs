afc8e103c33fb84bbf0b246ebac9f8166fe7ea72
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-22 23:12:06 +0900

    [update] 敵AIの基底クラスにフィード変数・初期化処理を追加

--- Assets/Scripts/Enemy/AI/EnemyAIBase.cs
+++ Assets/Scripts/Enemy/AI/EnemyAIBase.cs
@@ -1,6 +1,9 @@
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using Enemy.AsyncNode;
+using Enemy.Handler;
 using Player;
+using SO.Enemy;
 using UnityEngine;
 
 namespace Enemy.AI
@@ -8,6 +11,29 @@ namespace Enemy.AI
     /// <summary>敵AIの基底クラス</summary>
     public abstract class EnemyAIBase : MonoBehaviour
     {
+        /// <summary>プレイヤーの制御クラス</summary>
+        protected PlayerController Player;
+        /// <summary>ステータス情報</summary>
+        protected EnemyStatusData StatusData;
+        /// <summary>現在の体力</summary>
+        protected float CurrentHp;
+        /// <summary>現在の攻撃ヒット数</summary>
+        protected int CurrentHitCount;
+        /// <summary>BTのキャンセルトークン</summary>
+        protected CancellationTokenSource Cts;
+        /// <summary>BTのルートノード</summary>
+        protected BaseAsyncNode MainNode;
+        /// <summary>アニメーション制御クラス</summary>
+        protected EnemyAnimationHandler AnimationHandler;
+        /// <summary>剛体</summary>
+        protected Rigidbody Rigidbody;
+
+        private void Awake()
+        {
+            AnimationHandler = GetComponent<EnemyAnimationHandler>();
+            Rigidbody = GetComponent<Rigidbody>();
+        }
+        
         /// <summary>ビヘイビアツリーを開始する</summary>
         public abstract void BeginBehaviourTree();
 
@@ -15,24 +41,20 @@ namespace Enemy.AI
         protected abstract UniTask ExecuteBehaviourTree();
 
         /// <summary>ビヘイビアツリーを構築する</summary>
-        protected abstract void ConstructBehaviourTree();
+        public abstract void ConstructBehaviourTree();
 
-        /// <summary>攻撃シーケンスを構築する</summary>
-        protected abstract AsyncSequenceNode ConstructAttackSequence();
-
-        /// <summary>プレイヤーに攻撃が命中した時の処理</summary>
+        /// <summary>攻撃がプレイヤーに命中した際の処理</summary>
         public abstract void OnHitPlayer(PlayerController player);
 
-        /// <summary>プレイヤーの攻撃が命中した時の処理</summary>
-        public abstract void OnHit(float damage, Vector3 hitPosition);
-        
-        /// <summary>初期化する</summary>
-        public abstract void Initialize();
-        
-        /// <summary>プレイヤーを設定する</summary>
-        public abstract void SetPlayer(PlayerController player);
+        /// <summary>プレイヤーの攻撃が命中した際の処理</summary>
+        public abstract void OnHitByPlayer(float damage, Vector3 hitPosition);
 
-        /// <summary>ステータスを設定する</summary>
-        protected abstract void SetStatus();
+        /// <summary>初期化する</summary>
+        public void Initialize(PlayerController player, EnemyStatusData data)
+        {
+            StatusData = data;
+            Player = player;
+            CurrentHp = data.maxHp;
+        }
     }
 }
