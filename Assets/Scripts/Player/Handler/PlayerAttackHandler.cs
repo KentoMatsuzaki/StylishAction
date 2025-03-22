28ea0fcae36ec4bbe19c3c558707105ed2e393d1
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-23 06:05:07 +0900

    [update] 攻撃クラスのリファクタリングを反映

--- Assets/Scripts/Player/Handler/PlayerAttackHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerAttackHandler.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Common.Attacker;
+using UnityEngine.Serialization;
 
 namespace Player.Handler
 {
     /// <summary>プレイヤーの攻撃を制御するクラス</summary>
     public class PlayerAttackHandler : MonoBehaviour
     {
-        [Header("武器による攻撃の派生クラスのリスト"), SerializeField] 
-        private List<PlayerWeaponAttacker> weaponAttackerList;
+        [Header("攻撃クラスのリスト"), SerializeField] 
+        private List<PlayerAttacker> attackerList;
 
         /// <summary>コライダーを有効化する</summary>
         /// <param name="attackNumber">攻撃の番号(1~)</param>
@@ -17,7 +18,7 @@ namespace Player.Handler
             // 攻撃の番号が正常でない場合は処理を抜ける
             if (!IsValidAttackNumber(attackNumber)) return;
             // 攻撃コライダーを有効化する
-            weaponAttackerList[attackNumber - 1].EnableCollider();
+            attackerList[attackNumber - 1].EnableCollider();
         }
 
         /// <summary>コライダーを無効化する</summary>
@@ -27,13 +28,13 @@ namespace Player.Handler
             // 攻撃の番号が正常出ない場合は処理を抜ける
             if (!IsValidAttackNumber(attackNumber)) return;
             // 攻撃コライダーを無効化する
-            weaponAttackerList[attackNumber - 1].DisableCollider();
+            attackerList[attackNumber - 1].DisableCollider();
         }
 
         /// <summary>正しい攻撃の番号であるか</summary>
         private bool IsValidAttackNumber(int attackNumber)
         {
-            if (attackNumber < 1 || attackNumber > weaponAttackerList.Count)
+            if (attackNumber < 1 || attackNumber > attackerList.Count)
             {
                 Debug.LogWarning($"Invalid Attack Number : {attackNumber}"); return false;
             }
