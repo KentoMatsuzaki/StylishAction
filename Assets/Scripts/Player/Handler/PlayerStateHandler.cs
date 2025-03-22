4ea6f52f093c293863c31af738d5ace759230265
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-23 06:05:30 +0900

    [add] 移動と攻撃のフラグを追加

--- Assets/Scripts/Player/Handler/PlayerStateHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerStateHandler.cs
@@ -17,15 +17,43 @@ namespace Player.Handler
         }
 
         /// <summary>現在の状態を設定する</summary>
-        public void SetState(PlayerEnum.PlayerState state)
+        public void SetCurrentState(PlayerEnum.PlayerState state)
         {
             _currentState = state;
         }
 
         /// <summary>現在の状態を取得する</summary>
-        public PlayerEnum.PlayerState GetState()
+        public PlayerEnum.PlayerState GetCurrentState()
         {
             return _currentState;
+        }
+        
+        /// <summary>移動可能かどうか</summary>
+        public bool CanMove()
+        {
+            return _currentState is PlayerEnum.PlayerState.Idle or PlayerEnum.PlayerState.Move;
+        }
+
+        /// <summary>攻撃可能かどうか</summary>
+        public bool CanAttack()
+        {
+            return _currentState is PlayerEnum.PlayerState.Idle;
+        }
+        
+        //-------------------------------------------------------------------------------
+        // アニメーションイベント
+        //-------------------------------------------------------------------------------
+
+        /// <summary>Idleアニメーションから呼ばれる</summary>
+        public void SetStateIdle()
+        {
+            _currentState = PlayerEnum.PlayerState.Idle;
+        }
+
+        /// <summary>Parryアニメーションから呼ばれる</summary>
+        public void SetStateParry()
+        {
+            _currentState = PlayerEnum.PlayerState.Parry;
         }
     }
 }
