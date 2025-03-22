a509ef7aeadd603a9266622b7256ba12575c5ded
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-22 23:13:14 +0900

    [update] 敵のステータスをフェーズごとの項目に変更

--- Assets/Scripts/SO/Enemy/EnemyStatusData.cs
+++ Assets/Scripts/SO/Enemy/EnemyStatusData.cs
@@ -1,19 +1,13 @@
 using UnityEngine;
 
-namespace Data.Enemy
+namespace SO.Enemy
 {
     /// <summary>敵のステータス情報を保持するスクリプタブルオブジェクト</summary>
     [CreateAssetMenu(fileName = "EnemyStatus", menuName = "ScriptableObjects/CreateEnemyStatusAsset")]
     public class EnemyStatusData : ScriptableObject
     {
-        /// <summary>フェーズ1の最大体力</summary>
-        public float phase1MaxHp;
-        
-        /// <summary>フェーズ2の最大体力</summary>
-        public float phase2MaxHp;
-        
-        /// <summary>フェーズ3の最大体力</summary>
-        public float phase3MaxHp;
+        /// <summary>最大体力</summary>
+        public float maxHp;
         
         /// <summary>移動速度</summary>
         public float moveSpeed;
