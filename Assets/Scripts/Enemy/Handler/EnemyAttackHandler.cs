80937879ee572607be570383331ca4a224f8c795
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-03-23 06:06:36 +0900

    [add] Meteorのコライダーに関する処理を追加

--- Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
+++ Assets/Scripts/Enemy/Handler/EnemyAttackHandler.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Common.Attacker;
 using UnityEngine;
 
@@ -6,18 +9,38 @@ namespace Enemy.Handler
     /// <summary>敵の攻撃を制御するクラス</summary>
     public class EnemyAttackHandler : MonoBehaviour
     {
-        [Header("武器による攻撃の派生クラス"), SerializeField] private EnemyWeaponAttacker weaponAttacker;
+        [Header("攻撃クラスのリスト"), SerializeField] private List<EnemyAttacker> attackerList;
         
         /// <summary>コライダーを有効化する</summary>
-        public void EnableCollider()
+        /// <param name="attackName">攻撃の種類</param>
+        public void EnableCollider(string attackName)
         {
-            weaponAttacker.EnableCollider();
+            attackerList.FirstOrDefault(a => a.type.ToString() == attackName)?.EnableCollider();
         }
 
         /// <summary>コライダーを無効化する</summary>
-        public void DisableCollider()
+        /// <param name="attackName">攻撃の種類</param>
+        public void DisableCollider(string attackName)
         {
-            weaponAttacker.DisableCollider();
+            attackerList.FirstOrDefault(a => a.type.ToString() == attackName)?.DisableCollider();
+        }
+        
+        //-------------------------------------------------------------------------------
+        // アニメーションイベント
+        //-------------------------------------------------------------------------------
+        
+        /// <summary>Meteorのアニメーションイベントから呼ばれる</summary>
+        public void EnableMeteorAndDisableMeteorColliderWithDelay()
+        {
+            StartCoroutine(EnableAndDisableMeteorColliderRoutine());
+        }
+        
+        private IEnumerator EnableAndDisableMeteorColliderRoutine()
+        {
+            yield return new WaitForSeconds(0.5f);
+            EnableCollider("Meteor");
+            yield return new WaitForSeconds(0.1f);
+            DisableCollider("Meteor");
         }
     }
 }
