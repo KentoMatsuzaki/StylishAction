f795db55a4a54352ed153e49a1c1a6b0898b216f
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-04-07 13:52:03 +0900

    [update] 三人称視点の軌道カメラを実装。

--- Assets/Scripts/Camera/OrbitCamera.cs
+++ Assets/Scripts/Camera/OrbitCamera.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Camera
 {
@@ -14,31 +15,50 @@ namespace Camera
         public float moveSpeed;   // 移動速度
         public float rotateSpeed; // 回転速度
         public float maxDistance; // 障害物を判定する最長距離
+
+        private Vector2 _mouseDelta;
         
+        //-------------------------------------------------------------------------------
+        // 更新処理
+        //-------------------------------------------------------------------------------
+
         private void FixedUpdate()
         {
             // 位置を更新
-            UpdateCameraPosition();
-            // 
-            UpdateCameraRotation();
-            
-            //AdjustCameraPosition();
+            //UpdateCameraPosition();
+            // 回転を更新
+            //UpdateCameraRotation();
         }
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _mouseDelta.x * rotateSpeed, Space.World);
+        }
+        
+        //-------------------------------------------------------------------------------
+        // 視点移動のコールバック
+        //-------------------------------------------------------------------------------
+
+        /// <summary>PlayerInputから呼ばれる</summary>
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            _mouseDelta = context.ReadValue<Vector2>();
+        }
+        
+        //-------------------------------------------------------------------------------
+        // カメラに関する処理
+        //-------------------------------------------------------------------------------
 
         /// <summary>カメラの位置を更新する</summary>
         private void UpdateCameraPosition()
         {
-            var targetPosition = player.position + offset;
-            transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            
         }
 
         /// <summary>カメラの回転を更新する</summary>
         private void UpdateCameraRotation()
         {
-            var targetPosition = new Vector3(enemy.position.x, enemy.position.y + 1f, enemy.position.z);
-            var targetDirection = (targetPosition - transform.position).normalized;
-            var lookRotation = Quaternion.LookRotation(targetDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, Time.deltaTime * rotateSpeed);
         }
 
         /// <summary>カメラの位置を調整する</summary>
