ee4989720e58689097c288d2fa9f0570e36dcce9
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-20 22:19:21 +0900

    [update] 移動・回転の処理を修正

--- Assets/Scripts/Player/Handler/PlayerLocomotionHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerLocomotionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Player.Handler
 {
@@ -6,7 +7,7 @@ namespace Player.Handler
     public class PlayerLocomotionHandler : MonoBehaviour
     {
         /// <summary>移動方向</summary>
-        private Vector3 _moveDirection;
+        public Vector3 MoveDirection { get; set; }
         
         private Rigidbody _rb;
         
@@ -20,26 +21,28 @@ namespace Player.Handler
         }
         
         //-------------------------------------------------------------------------------
-        // 更新処理
+        // 移動処理
         //-------------------------------------------------------------------------------
-
+        
         /// <summary>移動方向を設定する</summary>
         /// <param name="moveInput">移動の入力値</param>
         public void SetMoveDirection(Vector2 moveInput)
         {
             // 移動の入力値をVector3に変換して代入する
-            _moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
+            MoveDirection = new Vector3(moveInput.x, 0, moveInput.y);
         }
-        
-        //-------------------------------------------------------------------------------
-        // 移動処理
-        //-------------------------------------------------------------------------------
 
         /// <summary>入力に基づいて計算された移動方向に、指定された速度で移動させる</summary>
         /// <param name="moveSpeed">移動速度</param>
-        public void HandleMovement(float moveSpeed)
+        public void MoveInputDirection(float moveSpeed)
         {
-            transform.Translate(Time.deltaTime * moveSpeed * _moveDirection, Space.Self);
+            transform.Translate(Time.deltaTime * moveSpeed * MoveDirection, Space.Self);
+        }
+
+        /// <summary>正面方向に、指定された速度で移動させる</summary>
+        public void MoveForward(float moveSpeed)
+        {
+            transform.Translate(Time.deltaTime * moveSpeed * transform.forward, Space.World);
         }
         
         //-------------------------------------------------------------------------------
@@ -58,11 +61,22 @@ namespace Player.Handler
             transform.rotation = Quaternion.LookRotation(cameraForward);
         }
 
-        /// <summary>正面方向にダッシュさせる</summary>
-        /// <param name="dashSpeed">ダッシュ速度</param>
-        public void DashForward(float dashSpeed)
+        /// <summary>カメラを基準とした入力方向へ回転させる</summary>
+        /// <param name="cameraTransform">カメラの位置</param>
+        public void RotateTowardsCameraRelativeDirection(Transform cameraTransform)
         {
-            _rb.AddForce(transform.forward * dashSpeed, ForceMode.Impulse);
+            // カメラの正面方向を取得する
+            var cameraForward = cameraTransform.forward.normalized;
+            // 高さを無視する
+            cameraForward.y = 0;
+            // カメラの右方向を取得する
+            var cameraRight = cameraTransform.right.normalized;
+            // 高さを無視する
+            cameraRight.y = 0;
+            // 回転方向を計算する
+            var rotation = cameraForward * MoveDirection.z + cameraRight * MoveDirection.x;
+            // 回転させる
+            transform.rotation = Quaternion.LookRotation(rotation);
         }
     }
 }
