400049b7adf0fb0709f87811371042d6275dfdfa
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-20 14:51:13 +0900

    [update] ActionMapのアクション名を更新

--- Assets/Scripts/Player/Handler/PlayerLocomotionHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerLocomotionHandler.cs
@@ -19,20 +19,16 @@ namespace Player.Handler
             transform.Translate(Time.deltaTime * moveSpeed * transform.forward, Space.World);
         }
 
-        /// <summary>カメラの向きを基準に、入力方向へ回転させる</summary>
-        /// <param name="moveInput">入力方向</param>
+        /// <summary>カメラの正面方向へ回転させる</summary>
         /// <param name="cameraTransform">カメラの位置</param>
-        public void RotateToInputRelativeToCamera(Vector2 moveInput, Transform cameraTransform)
+        public void RotateTowardsCameraForward(Transform cameraTransform)
         {
             // カメラの正面方向を取得する
             var cameraForward = cameraTransform.forward.normalized;
+            // 高さを無視する
             cameraForward.y = 0;
-            // カメラの右方向を取得する
-            var cameraRight = cameraTransform.right.normalized;
-            cameraRight.y = 0;
-            // 回転方向を計算する
-            var moveDirection = cameraForward * moveInput.y + cameraRight * moveInput.x;
-            transform.rotation = Quaternion.LookRotation(moveDirection);
+            // カメラの正面方向に回転させる
+            transform.rotation = Quaternion.LookRotation(cameraForward);
         }
 
         /// <summary>正面方向にダッシュさせる</summary>
