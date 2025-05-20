b4d5657b2fe12565e3e569900bf02ae9a0225c09
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-20 14:52:21 +0900

    [refactor] 移動のハンドラーを移動＋回転のハンドラーに変更。移動・回転処理を修正。

--- Assets/Scripts/Player/Handler/PlayerLocomotionHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerLocomotionHandler.cs
@@ -2,22 +2,49 @@ using UnityEngine;
 
 namespace Player.Handler
 {
-    /// <summary>プレイヤーの移動を制御するクラス</summary>
-    public class PlayerMoveHandler : MonoBehaviour
+    /// <summary>プレイヤーの移動と回転を制御するクラス</summary>
+    public class PlayerLocomotionHandler : MonoBehaviour
     {
+        /// <summary>移動方向</summary>
+        private Vector3 _moveDirection;
+        
         private Rigidbody _rb;
+        
+        //-------------------------------------------------------------------------------
+        // 初期設定
+        //-------------------------------------------------------------------------------
 
         private void Awake()
         {
             _rb = GetComponent<Rigidbody>();
         }
+        
+        //-------------------------------------------------------------------------------
+        // 更新処理
+        //-------------------------------------------------------------------------------
 
-        /// <summary>正面方向に移動させる</summary>
-        /// <param name="moveSpeed">移動速度</param>
-        public void MoveForward(float moveSpeed)
+        /// <summary>移動方向を設定する</summary>
+        /// <param name="moveInput">移動の入力値</param>
+        public void SetMoveDirection(Vector2 moveInput)
         {
-            transform.Translate(Time.deltaTime * moveSpeed * transform.forward, Space.World);
+            // 移動の入力値をVector3に変換して代入する
+            _moveDirection = new Vector3(moveInput.x, 0, moveInput.y);
         }
+        
+        //-------------------------------------------------------------------------------
+        // 移動処理
+        //-------------------------------------------------------------------------------
+
+        /// <summary>入力に基づいて計算された移動方向に、指定された速度で移動させる</summary>
+        /// <param name="moveSpeed">移動速度</param>
+        public void HandleMovement(float moveSpeed)
+        {
+            transform.Translate(Time.deltaTime * moveSpeed * _moveDirection, Space.Self);
+        }
+        
+        //-------------------------------------------------------------------------------
+        // 回転処理
+        //-------------------------------------------------------------------------------
 
         /// <summary>カメラの正面方向へ回転させる</summary>
         /// <param name="cameraTransform">カメラの位置</param>
