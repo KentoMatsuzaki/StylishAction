737614445ca3e221d648411f7bd465cad1fc7f7f
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-22 03:53:41 +0900

    [add] 状態遷移の制限を実装。移動入力の制限を実装。

--- Assets/Scripts/Player/Handler/PlayerStateHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerStateHandler.cs
@@ -21,6 +21,18 @@ namespace Player.Handler
         /// <summary>回避状態</summary>
         public PlayerState DodgeState { get; private set; }
         
+        /// <summary>通常攻撃状態</summary>
+        public PlayerState AttackNormalState { get; private set; }
+        
+        /// <summary>特殊攻撃状態</summary>
+        public PlayerState AttackSpecialState { get; private set; }
+        
+        /// <summary>空中攻撃状態</summary>
+        public PlayerState AttackAerialState { get; private set; }
+        
+        /// <summary>EX攻撃状態</summary>
+        public PlayerState AttackExtraState { get; private set; }
+        
         //-------------------------------------------------------------------------------
         // 初期設定
         //-------------------------------------------------------------------------------
@@ -28,16 +40,68 @@ namespace Player.Handler
         private void Awake()
         {
             CreateAllStates();
+            DefineInvalidTransitions();
             Initialize();
         }
 
         /// <summary>全ての状態を作成する</summary>
         private void CreateAllStates()
         {
+            // 静止状態
             IdleState = new PlayerState(PlayerEnum.EPlayerState.Idle);
+            
+            // 移動状態
             MoveState = new PlayerState(PlayerEnum.EPlayerState.Move);
+            
+            // スプリント状態
             SprintState = new PlayerState(PlayerEnum.EPlayerState.Sprint);
+            
+            // 回避状態
             DodgeState = new PlayerState(PlayerEnum.EPlayerState.Dodge);
+            
+            // 通常攻撃状態
+            AttackNormalState = new PlayerState(PlayerEnum.EPlayerState.AttackNormal);
+            
+            // 特殊攻撃状態
+            AttackSpecialState = new PlayerState(PlayerEnum.EPlayerState.AttackSpecial);
+            
+            // 空中攻撃状態
+            AttackAerialState = new PlayerState(PlayerEnum.EPlayerState.AttackAerial);
+            
+            // 必殺攻撃状態
+            AttackExtraState = new PlayerState(PlayerEnum.EPlayerState.AttackExtra);
+        }
+
+        /// <summary>各状態ごとに遷移できない状態を定義する</summary>
+        private void DefineInvalidTransitions()
+        {
+            // 移動状態
+            MoveState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackNormal);
+            MoveState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackSpecial);
+            MoveState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackAerial);
+            MoveState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackExtra);
+            
+            // スプリント状態
+            SprintState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackNormal);
+            SprintState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackSpecial);
+            SprintState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackAerial);
+            SprintState.InvalidTransitions.Add(PlayerEnum.EPlayerState.AttackExtra);
+            
+            // 通常攻撃状態
+            AttackNormalState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Move);
+            AttackNormalState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Sprint);
+            
+            // 特殊攻撃状態
+            AttackSpecialState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Move);
+            AttackSpecialState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Sprint);
+            
+            // 空中攻撃状態
+            AttackAerialState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Move);
+            AttackAerialState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Sprint);
+            
+            // 必殺攻撃状態
+            AttackExtraState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Move);
+            AttackExtraState.InvalidTransitions.Add(PlayerEnum.EPlayerState.Sprint);
         }
 
         /// <summary>状態の初期化を行う</summary>
@@ -55,23 +119,36 @@ namespace Player.Handler
         /// <param name="nextState">次の状態</param>
         public void SwitchState(PlayerState nextState)
         {
+            // 次の状態が存在しない場合はログを出力して処理を抜ける
+            if (nextState== null)
+            {
+                Debug.LogWarning("Next State Not Found");
+                return;
+            }
+            
+            // 次の状態へ現在の状態から遷移できない場合は処理を抜ける
+            if (CurrentState.InvalidTransitions.Contains(nextState.StateType))
+            {
+                Debug.Log("Invalid Transition");
+                return;
+            }
+            
+            // 現在の状態の終了時に呼ばれる処理を呼び出す
             if (CurrentState != null)
             {
                 CurrentState.Exit();
             }
             
-            if (nextState== null)
-            {
-                Debug.LogWarning("New state not found");
-                return;
-            }
-
-            
+            Debug.Log($"{CurrentState.StateType} to {nextState.StateType} {Time.time}");
             
             CurrentState = nextState;
             CurrentState.Enter();
-            
-            Debug.Log($"{CurrentState.State} {Time.time}");
+        }
+
+        /// <summary>移動入力を受け付けるかどうか</summary>
+        public bool CanAcceptMoveInput()
+        {
+            return CurrentState == IdleState || CurrentState == MoveState || CurrentState == SprintState;
         }
     }
 }
