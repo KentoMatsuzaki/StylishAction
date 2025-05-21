a8bbead05b39613c70f68960f97ad25ee3ab9cb9
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-21 15:42:35 +0900

    [add] 静止状態に切り替える処理を追加

--- Assets/Scripts/Player/Handler/PlayerModelEventHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerModelEventHandler.cs
@@ -6,11 +6,18 @@ namespace Player.Handler
     public class PlayerModelEventHandler : MonoBehaviour
     {
         private PlayerStateHandler _stateHandler;
+        private PlayerLocomotionHandler _locomotionHandler;
 
         private void Awake()
         {
             _stateHandler = GetComponentInParent<PlayerStateHandler>();
+            _locomotionHandler = GetComponentInParent<PlayerLocomotionHandler>();
         }
-        
+
+        /// <summary>静止状態に切り替える</summary>
+        public void SwitchStateToIdle()
+        {
+            _stateHandler.SwitchState(_stateHandler.IdleState);
+        }
     }
 }
