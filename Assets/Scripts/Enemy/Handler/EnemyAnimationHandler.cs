caa80941e8e1b95e372457430d78c460f4697132
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-24 20:40:58 +0900

    [refactor] 敵のアニメーションを制御するクラスを修正

--- Assets/Scripts/Enemy/Handler/EnemyAnimationHandler.cs
+++ Assets/Scripts/Enemy/Handler/EnemyAnimationHandler.cs
@@ -10,54 +10,58 @@ namespace Enemy.Handler
     public class EnemyAnimationHandler : MonoBehaviour
     { 
         private Animator _animator;
+        
+        //-------------------------------------------------------------------------------
+        // 初期設定
+        //-------------------------------------------------------------------------------
 
         private void Awake()
         {
-            _animator = GetComponent<Animator>();
-        }
-        
-        //-------------------------------------------------------------------------------
-        // 汎用処理
-        //-------------------------------------------------------------------------------
-
-        /// <summary>アニメーションを再生する</summary>
-        public void PlayAnimation(string stateName)
-        {
-            _animator.Play(stateName);
+            _animator = GetComponentInChildren<Animator>();
         }
         
         //-------------------------------------------------------------------------------
         // 移動に関する処理
         //-------------------------------------------------------------------------------
-
-        /// <summary>移動フラグを設定する</summary>
-        public void SetMoveFlag(bool value)
+        
+        /// <summary>移動のフラグを有効化する</summary>
+        public void EnableMove()
         {
-            if (_animator.GetBool(EnemyConst.IsMoving) != value)
-            {
-                _animator.SetBool(EnemyConst.IsMoving, value);
-            }
+            _animator.SetBool(EnemyConst.IsMoving, true);
+        }
+
+        /// <summary>移動のフラグを無効化する</summary>
+        public void DisableMove()
+        {
+            _animator.SetBool(EnemyConst.IsMoving, false);
         }
         
         //-------------------------------------------------------------------------------
         // 攻撃に関する処理
         //-------------------------------------------------------------------------------
         
-        /// <summary>攻撃アニメーションをトリガーする</summary>
-        /// <param name="type">スキルの種類</param>
-        public void TriggerAttack(InGameEnum.EnemyAttackType type)
+        /// <summary>攻撃のトリガーを有効化する</summary>
+        /// <param name="attackType">攻撃の種類</param>
+        public void TriggerAttack(EnemyEnum.AttackType attackType)
         {
-            _animator.SetTrigger(type.ToString());
+            _animator.SetTrigger(attackType.ToString());
         }
         
+        //-------------------------------------------------------------------------------
+        // 非同期処理
+        //-------------------------------------------------------------------------------
+        
         /// <summary>アニメーションの再生終了を待つ</summary>
-        public async UniTask WaitForAnimationEnd(CancellationToken token)
+        public async UniTask WaitUntilAnimationComplete(CancellationToken token)
         {
+            // 現在のステート情報を取得する
             var stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
-
+            // アニメーションを再生している場合
             while (stateInfo.normalizedTime < 1.0f || _animator.IsInTransition(0) || stateInfo.normalizedTime > 1.1f)
             {
+                // 次のフレームまで待機する
                 await UniTask.Yield(PlayerLoopTiming.Update, token);
+                // 現在のステート情報を更新する
                 stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
             }
         }
