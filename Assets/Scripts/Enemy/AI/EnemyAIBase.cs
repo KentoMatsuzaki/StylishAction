56469548734c111cb040d4ef2803cdde1f798fe1
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-24 20:41:42 +0900

    [add] 敵のAI挙動を制御する基底クラスを追加

--- Assets/Scripts/Enemy/AI/EnemyAIBase.cs
+++ Assets/Scripts/Enemy/AI/EnemyAIBase.cs
@@ -1,63 +1,56 @@
-using System.Threading;
 using Cysharp.Threading.Tasks;
-using Enemy.AsyncNode;
-using Enemy.Handler;
 using Player;
 using SO.Enemy;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Threading;
+using Enemy.AsyncNode;
+using Enemy.Handler;
 
 namespace Enemy.AI
 {
-    /// <summary>敵AIの基底クラス</summary>
+    /// <summary>敵のAI挙動を制御する抽象クラス</summary>
     public abstract class EnemyAIBase : MonoBehaviour
     {
-        /// <summary>プレイヤーの制御クラス</summary>
+        /// <summary>プレイヤー</summary>
         protected PlayerController Player;
-        /// <summary>ステータス情報</summary>
-        protected EnemyStatusData StatusData;
-        /// <summary>現在の体力</summary>
-        protected float CurrentHp;
-        /// <summary>現在の攻撃ヒット数</summary>
-        protected int CurrentHitCount;
-        /// <summary>BTのキャンセルトークン</summary>
-        protected CancellationTokenSource Cts;
-        /// <summary>BTのルートノード</summary>
-        protected BaseAsyncNode MainNode;
-        /// <summary>アニメーション制御クラス</summary>
-        protected EnemyAnimationHandler AnimationHandler;
-        /// <summary>剛体</summary>
-        protected Rigidbody Rigidbody;
-
-        private void Awake()
-        {
-            AnimationHandler = GetComponent<EnemyAnimationHandler>();
-            Rigidbody = GetComponent<Rigidbody>();
-        }
         
-        /// <summary>ビヘイビアツリーを開始する</summary>
-        public abstract void BeginBehaviourTree();
+        /// <summary>ステータス情報</summary>
+        protected EnemyStats Stats;
 
-        /// <summary>ビヘイビアツリーを実行する</summary>
-        protected abstract UniTask ExecuteBehaviourTree();
+        /// <summary>攻撃情報のリスト</summary>
+        protected List<EnemyAttackStats> AttackStatsList;
 
+        /// <summary>現在の攻撃情報</summary>
+        protected EnemyAttackStats CurrentAttackStats;
+        
+        /// <summary>ビヘイビアツリーの最上位ノード</summary>
+        protected BaseAsyncNode RootNode;
+
+        /// <summary>ビヘイビアツリーのキャンセルトークン</summary>
+        protected CancellationTokenSource Cts;
+
+        /// <summary>攻撃の制御クラス</summary>
+        protected EnemyAttackHandler AttackHandler;
+
+        //-------------------------------------------------------------------------------
+        // ビヘイビアツリーに関する処理
+        //-------------------------------------------------------------------------------
+        
         /// <summary>ビヘイビアツリーを構築する</summary>
-        public abstract void ConstructBehaviourTree();
+        protected abstract void BuildTree();
 
-        /// <summary>プレイヤーにパリィされた時の処理</summary>
-        public abstract void OnParried();
+        /// <summary>ビヘイビアツリーの評価を実行する</summary>
+        protected abstract UniTask Tick();
+        
+        /// <summary>ビヘイビアツリーの評価を開始する</summary>
+        protected abstract void StartBehaviour();
+        
+        //-------------------------------------------------------------------------------
+        // 攻撃に関する処理
+        //-------------------------------------------------------------------------------
 
-        /// <summary>攻撃がプレイヤーに命中した際の処理</summary>
-        public abstract void OnHitPlayer(PlayerController player);
-
-        /// <summary>プレイヤーの攻撃が命中した際の処理</summary>
-        public abstract void OnHitByPlayer(float damage, Vector3 hitPosition);
-
-        /// <summary>初期化する</summary>
-        public void Initialize(PlayerController player, EnemyStatusData data)
-        {
-            StatusData = data;
-            Player = player;
-            CurrentHp = data.maxHp;
-        }
+        /// <summary>攻撃の結果を適用する</summary>
+        public abstract void ApplyAttack();
     }
 }
