972fab67a356f2e071159b72bb78198f12814f0d
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-26 19:12:15 +0900

    [add] プレイヤーの攻撃を発生させるクラスを追加

---
+++ Assets/Scripts/Player/PlayerAttackInvoker.cs
@@ -0,0 +1,58 @@
+using Const;
+using DefaultNamespace;
+using Enemy.AI;
+using UnityEngine;
+using EnemyTest = DefaultNamespace.EnemyTest;
+
+namespace Player
+{
+    /// <summary>プレイヤーの攻撃を発生させるクラス</summary>
+    public class PlayerAttackInvoker : MonoBehaviour
+    {
+        /// <summary>プレイヤーの制御クラス</summary>
+        private PlayerController _player;
+        
+        /// <summary>攻撃の当たり判定</summary>
+        private Collider _collider;
+        
+        //-------------------------------------------------------------------------------
+        // 初期設定
+        //-------------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            // プレイヤーの制御クラスを取得する
+            _player = GetComponentInParent<PlayerController>();
+            // 攻撃の当たり判定を取得する
+            _collider = GetComponent<Collider>();
+            // 攻撃の当たり判定をトリガーにする
+            _collider.isTrigger = true;
+            // 攻撃の当たり判定を無効化する
+            _collider.enabled = false;
+        }
+        
+        //-------------------------------------------------------------------------------
+        // 攻撃に関する処理
+        //-------------------------------------------------------------------------------
+        
+        /// <summary>攻撃の当たり判定を有効化する</summary>
+        public void EnableCollider() => _collider.enabled = true;
+        
+        /// <summary>攻撃の当たり判定を無効化する</summary>
+        public void DisableCollider() => _collider.enabled = false;
+
+        /// <summary>攻撃の当たり判定による接触イベント</summary>
+        private void OnTriggerEnter(Collider other)
+        {
+            // プレイヤーと接触した場合
+            if (other.CompareTag(EnemyConst.GameObjectTag))
+            {
+                // 敵AIの制御クラスを取得する
+                //var enemy = other.GetComponent<EnemyAIBase>();
+                // 敵AIのダメージ適用処理を呼び出す
+                //enemy.ApplyDamage(_player.CurrentAttackStats);
+                other.GetComponent<EnemyTest>().Test();
+            }
+        }
+    }
+}
