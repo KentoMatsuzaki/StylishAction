9af071bd69fcd133bd424d6b238beb42ad05e6d9
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-26 19:15:15 +0900

    [add] プレイヤーの攻撃コライダーを有効化させるアニメーションイベントを追加

--- Assets/Scripts/Player/Handler/PlayerModelEventHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerModelEventHandler.cs
@@ -1,3 +1,4 @@
+using Enum;
 using Player.Interface;
 using UnityEngine;
 
@@ -24,6 +25,18 @@ namespace Player.Handler
         public void ApplyAttackSpecial4Force()
         {
             _animationEventHandler.ApplyAttackSpecial4Force();
+        }
+        
+        /// <summary>右手の攻撃の当たり判定を持続時間だけ有効化する</summary>
+        public void EnableRightAttackColliderForDuration()
+        {
+            _animationEventHandler.EnableRightAttackColliderForDuration();
+        }
+
+        /// <summary>左手の攻撃の当たり判定を持続時間だけ有効化する</summary>
+        public void EnableLeftAttackColliderForDuration()
+        {
+            _animationEventHandler.EnableLeftAttackColliderForDuration();
         }
     }
 }
