449ee01817c2f218560a39926b94880a8b685f61
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-26 19:13:03 +0900

    [add] プレイヤーに関する列挙型を保持するクラスを追加

---
+++ Assets/Scripts/Enum/PlayerEnums.cs
@@ -0,0 +1,39 @@
+namespace Enum
+{
+    /// <summary>プレイヤーに関する列挙型を保持するクラス</summary>
+    public static class PlayerEnums
+    {
+        //-------------------------------------------------------------------------------
+        // 状態に関する列挙型
+        //-------------------------------------------------------------------------------
+        
+        /// <summary>プレイヤーの状態</summary>
+        public enum PlayerState
+        {
+            Idle,          // 静止
+            Move,          // 移動
+            Sprint,        // スプリント
+            Dodge,         // 回避
+            AttackNormal,  // 通常攻撃
+            AttackSpecial, // 特殊攻撃
+            AttackExtra,   // EX攻撃
+            Transition,    // 遷移
+            Parry,         // パリィ
+            Guard,         // 防御
+            Damage,        // 被弾
+            Death          // 死亡
+        }
+        
+        //-------------------------------------------------------------------------------
+        // 攻撃に関する列挙型
+        //-------------------------------------------------------------------------------
+
+        /// <summary>攻撃の種類</summary>
+        public enum AttackType
+        {
+            RightSword,
+            LeftSword,
+            ExtraAttack
+        }
+    }
+}
