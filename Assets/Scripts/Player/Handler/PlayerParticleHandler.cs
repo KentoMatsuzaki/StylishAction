188cd65a1bfb95cb2cdeb621131df14ed15ec7d8
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-26 19:18:49 +0900

    [update] プレイヤーのパーティクルを制御するクラスを修正

--- Assets/Scripts/Player/Handler/PlayerParticleHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerParticleHandler.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 namespace Player.Handler
 {
-    /// <summary>プレイヤーのパーティクル処理を仲介するクラス</summary>
+    /// <summary>プレイヤーのパーティクルを制御するクラス</summary>
     public class PlayerParticleHandler : MonoBehaviour
     {
         /// <summary>パーティクルを有効化する</summary>
         /// <param name="particleType">パーティクルの種類</param>
-        public void ActivateParticle(string particleType)
+        public void ActivateParticle(ParticleEnums.PlayerParticleType particleType)
         {
-            if (System.Enum.TryParse<InGameEnum.PlayerParticleType>(particleType, out var type))
-            {
-                ParticleManager.Instance.ActivatePlayerParticle(type);
-            }
-            else
-            {
-                Debug.LogWarning($"Particle not found : {particleType}");
-            }
+            ParticleManager.Instance.ActivatePlayerParticle(particleType);
+        }
+
+        /// <summary>パーティクルを無効化する</summary>
+        /// <param name="particleType">パーティクルの種類</param>
+        public void DeactivateParticle(ParticleEnums.PlayerParticleType particleType)
+        {
+            ParticleManager.Instance.DeactivatePlayerParticle(particleType);
         }
     }
 }
