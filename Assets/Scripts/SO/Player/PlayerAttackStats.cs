6b30d8f29c46fd0b7cee4e63f0cfa4a613ad46aa
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-26 19:16:13 +0900

    [add] 攻撃の持続時間・攻撃時に敵の方向へ回転させる速度のパラメーターを追加

--- Assets/Scripts/SO/Player/PlayerAttackStats.cs
+++ Assets/Scripts/SO/Player/PlayerAttackStats.cs
@@ -8,9 +8,15 @@ namespace SO.Player
     public class PlayerAttackStats : ScriptableObject
     {
         /// <summary>攻撃の種類</summary>
-        public InGameEnum.PlayerAttackType type;
+        public PlayerEnums.AttackType type;
 
         /// <summary>ダメージ量</summary>
         public float attackDamage;
+
+        /// <summary>持続時間</summary>
+        public float attackDuration;
+
+        /// <summary>攻撃時に敵の方向へ回転する速度</summary>
+        public float attackRotationSpeed;
     }
 }
