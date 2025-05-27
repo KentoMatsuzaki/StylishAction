79e80676aba2adb782daa39d785c147ab355415e
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-27 16:18:16 +0900

    [update] パーティクルの種類を表す列挙型を、パーティクルと紐づいている攻撃の種類を表す列挙型に変更

--- Assets/Scripts/Enum/ParticleEnums.cs
+++ Assets/Scripts/Enum/ParticleEnums.cs
@@ -3,19 +3,25 @@ namespace Enum
     /// <summary>パーティクルに関する列挙型を保持するクラス</summary>
     public static class ParticleEnums
     {
-        /// <summary>プレイヤーのパーティクルの種類</summary>
-        public enum PlayerParticleType
+        /// <summary>パーティクルが紐づいている攻撃の種類</summary>
+        public enum ParticleAttackType
         {
-            AttackSpecialRight,
-            AttackSpecialLeft,
-            AttackExtra
-        }
-
-        /// <summary>敵のパーティクルの種類</summary>
-        public enum EnemyParticleType
-        {
-            Meteor,
-            Seraphic,
+            // プレイヤーの攻撃（1~100）
+            None = 0,
+            AttackNormal1 = 1,
+            AttackNormal2 = 2,
+            AttackNormal3 = 3,
+            AttackNormal4 = 4,
+            AttackNormal5 = 5,
+            AttackSpecial1 = 6,
+            AttackSpecial2 = 7,
+            AttackSpecial3 = 8,
+            AttackExtra = 9,
+            
+            // 敵の攻撃（101~200）
+            Scythe = 101,
+            Meteor = 102,
+            Seraph = 103,
         }
     }
 }
