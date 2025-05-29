f35d0bea7a3e4fdf1cfdd21e0d71baf663390d56
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-29 14:34:31 +0900

    [update] 敵AIの回転処理を調整

--- Assets/Scripts/Enemy/AI/DeathAI.cs
+++ Assets/Scripts/Enemy/AI/DeathAI.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks;
 using Enemy.AsyncNode;
 using Enemy.Handler;
 using Enum;
+using Particle;
 using Player;
 using SO.Player;
 
@@ -20,6 +21,12 @@ namespace Enemy.AI
         private void Start()
         {
             AttackHandler = GetComponent<EnemyAttackHandler>();
+            AnimationHandler = GetComponent<EnemyAnimationHandler>();
+            Rb = GetComponent<Rigidbody>();
+            CurrentHp = stats.maxHp;
+            CurrentPoise = stats.maxPoise;
+            BuildTree();
+            StartBehaviour();
         }
 
         //-------------------------------------------------------------------------------
@@ -89,7 +96,7 @@ namespace Enemy.AI
                 SelectCurrentAttackStats();
 
                 // プレイヤーを攻撃できる場合
-                if (AttackHandler.CanAttackPlayer(Player, CurrentAttackStats))
+                if (AttackHandler.CanAttackPlayer(player, CurrentAttackStats))
                 {
                     // 移動アニメーションのフラグを無効化する
                     AnimationHandler.DisableMove();
@@ -104,24 +111,29 @@ namespace Enemy.AI
                     // 評価結果を返す
                     return EnemyEnums.NodeStatus.Success;
                 }
-
+                
                 // 移動アニメーションのフラグを有効化する
                 AnimationHandler.EnableMove();
-                // プレイヤーの方向へ回転させる
-                AttackHandler.RotateTowardsPlayer(Player, Stats.rotateSpeed);
+
+                // プレイヤーが攻撃の有効角度内にいない場合
+                if (!AttackHandler.IsPlayerInAttackAngle(player, CurrentAttackStats))
+                {
+                    // プレイヤーの方向へ回転させる
+                    AttackHandler.RotateTowardsPlayer(player, stats.rotateSpeed);
+                }
                     
                 // プレイヤーが攻撃の最小有効射程よりも近くにいる場合
-                if (AttackHandler.IsPlayerTooClose(Player, CurrentAttackStats))
+                if (AttackHandler.IsPlayerTooClose(player, CurrentAttackStats))
                 {
                     // プレイヤーの逆方向へ移動させる
-                    AttackHandler.MoveAwayFromPlayer(Player, Stats.moveSpeed);
+                    AttackHandler.MoveAwayFromPlayer(player, stats.moveSpeed);
                 }
                     
                 // プレイヤーが攻撃の最大有効射程よりも遠くにいる場合
-                if (AttackHandler.IsPlayerTooFar(Player, CurrentAttackStats))
+                if (AttackHandler.IsPlayerTooFar(player, CurrentAttackStats))
                 {
                     // プレイヤーの方向へ移動させる
-                    AttackHandler.MoveTowardsPlayer(Player, Stats.moveSpeed);
+                    AttackHandler.MoveTowardsPlayer(player, stats.moveSpeed);
                 }
                     
                 // 評価結果を返す
@@ -135,7 +147,7 @@ namespace Enemy.AI
             // 攻撃情報が既に設定されている場合は処理を抜ける
             if (CurrentAttackStats != null) return;
             // 攻撃情報をランダムに選択する
-            CurrentAttackStats = AttackStatsList[UnityEngine.Random.Range(0, AttackStatsList.Count)];
+            CurrentAttackStats = attackStatsList[UnityEngine.Random.Range(0, attackStatsList.Count)];
         }
         
         //-------------------------------------------------------------------------------
@@ -168,7 +180,7 @@ namespace Enemy.AI
             // スタンのアニメーションを再生する
             AnimationHandler.PlayStunAnimation();
             // スタンの終了を待機する
-            await UniTask.Delay(TimeSpan.FromSeconds(Stats.stunDuration));
+            await UniTask.Delay(TimeSpan.FromSeconds(stats.stunDuration));
             // ビヘイビアツリーの評価を開始する
             StartBehaviour();
         }
@@ -246,9 +258,9 @@ namespace Enemy.AI
         private void ApplyKnockBack()
         {
             // プレイヤーの方向へ回転させる
-            AttackHandler.RotateTowardsPlayer(Player, Stats.rotateSpeed);
+            AttackHandler.RotateTowardsPlayer(player, stats.rotateSpeed);
             // プレイヤーの逆方向へ力を加える
-            Rb.AddForce(AttackHandler.GetFlatDirectionToPlayer(Player) * Stats.knockBackPower, ForceMode.Impulse);
+            Rb.AddForce(AttackHandler.GetFlatDirectionToPlayer(player) * stats.knockBackPower, ForceMode.Impulse);
         }
         
         //-------------------------------------------------------------------------------
@@ -258,7 +270,24 @@ namespace Enemy.AI
         /// <summary>靭性値を最大まで回復させる</summary>
         public void RestoreMaxPoise()
         {
-            CurrentPoise = Stats.maxPoise;
+            CurrentPoise = stats.maxPoise;
         }
+
+        /// <summary></summary>
+        public void ActivateMeteorParticle()
+        {
+            ParticleManager.Instance.ActivateParticle(ParticleEnums.ParticleType.Meteor);
+        }
+
+        public void ActivateSeraph1Particle()
+        {
+            ParticleManager.Instance.ActivateParticle(ParticleEnums.ParticleType.Seraph1);
+        }
+
+        public void ActivateSeraph2Particle()
+        {
+            ParticleManager.Instance.ActivateParticle(ParticleEnums.ParticleType.Seraph2);
+        }
+        
     }
 }
