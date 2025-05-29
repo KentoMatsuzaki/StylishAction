7dbf8ae496c2a5aaa588bf6dd09bbe6d49151755
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-29 14:43:02 +0900

    [del] プレイヤーの右手・左手の攻撃コライダーを有効化・無効化するアニメーションイベントを削除

--- Assets/Scripts/Player/Handler/PlayerModelEventHandler.cs
+++ Assets/Scripts/Player/Handler/PlayerModelEventHandler.cs
@@ -21,6 +21,7 @@ namespace Player.Handler
             _animationEventHandler.SwitchStateToIdle();
         }
 
+        
         public void SwitchStateToTransition()
         {
             _animationEventHandler.SwitchStateToTransition();
@@ -30,18 +31,6 @@ namespace Player.Handler
         public void ApplyAttackSpecial4Force()
         {
             _animationEventHandler.ApplyAttackSpecial4Force();
-        }
-        
-        /// <summary>右手の攻撃の当たり判定を持続時間だけ有効化する</summary>
-        public void EnableRightAttackColliderForDuration()
-        {
-            _animationEventHandler.EnableRightAttackColliderForDuration();
-        }
-
-        /// <summary>左手の攻撃の当たり判定を持続時間だけ有効化する</summary>
-        public void EnableLeftAttackColliderForDuration()
-        {
-            _animationEventHandler.EnableLeftAttackColliderForDuration();
         }
 
         public void ActivateAttackNormal1Particle()
--- Assets/Scripts/Player/Interface/IPlayerAnimationEventHandler.cs
+++ Assets/Scripts/Player/Interface/IPlayerAnimationEventHandler.cs
@@ -11,13 +11,6 @@ namespace Player.Interface
         /// <summary>特殊攻撃アニメーション4のアニメーションイベント</summary>
         void ApplyAttackSpecial4Force();
 
-        /// <summary>右手の攻撃の当たり判定を持続時間だけ有効化する</summary>
-        void EnableRightAttackColliderForDuration();
-
-        /// <summary>左手の攻撃の当たり判定を持続時間だけ有効化する</summary>
-        void EnableLeftAttackColliderForDuration();
-
-
         void ActivateAttackNormal1Particle();
 
         void ActivateAttackNormal2Particle();
