14344b838e8a4055e4a33a08659bc43417014b79
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-05-29 14:43:37 +0900

    [del] プレイヤーの攻撃情報から、攻撃の有効時間のフィールドを削除

--- Assets/Scripts/SO/Player/PlayerAttackStats.cs
+++ Assets/Scripts/SO/Player/PlayerAttackStats.cs
@@ -8,13 +8,10 @@ namespace SO.Player
     public class PlayerAttackStats : ScriptableObject
     {
         /// <summary>攻撃の種類</summary>
-        public PlayerEnums.AttackType type;
+        public PlayerEnums.AttackType attackType;
 
         /// <summary>ダメージ量</summary>
         public float attackDamage;
-
-        /// <summary>持続時間</summary>
-        public float attackDuration;
 
         /// <summary>攻撃時に敵の方向へ回転する速度</summary>
         public float attackRotationSpeed;
