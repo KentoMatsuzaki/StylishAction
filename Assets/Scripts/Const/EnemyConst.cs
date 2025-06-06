90693da757dab2add8f976839d3d75b12c90fde8
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-06-07 04:31:12 +0900

    [add] 静止・移動・スタン復帰のアニメーションステート名を表す定数を追加

--- Assets/Scripts/Const/EnemyConst.cs
+++ Assets/Scripts/Const/EnemyConst.cs
@@ -13,8 +13,11 @@ namespace Const
         // アニメーションステート
         //-------------------------------------------------------------------------------
         
+        public const string IdleState = "Idle"; // 静止
+        public const string MoveState = "Move"; // 移動
         public const string HitState = "Hit"; // 被弾
         public const string StunState = "Stun"; // スタン
+        public const string RecoverState = "Recover"; // スタン復帰
         public const string DieState = "Die"; // 死亡
         
         //-------------------------------------------------------------------------------
