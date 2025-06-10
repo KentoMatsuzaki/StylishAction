4d3bf0de1c57cca8799c6170e1722586fec2c622
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-06-10 19:18:51 +0900

    [add] SpとEpに関する処理を追加

--- Assets/Scripts/SO/Player/PlayerStats.cs
+++ Assets/Scripts/SO/Player/PlayerStats.cs
@@ -8,6 +8,33 @@ namespace SO.Player
     {
         /// <summary>最大体力</summary>
         public float maxHp;
+
+        /// <summary>最大スタミナ</summary>
+        public float maxSp = 1;
+
+        /// <summary>最大EXポイント</summary>
+        public float maxEp = 20;
+
+        /// <summary>スタミナ回復速度</summary>
+        public float sPRegenRate;
+
+        /// <summary>スプリントのスタミナ消費量</summary>
+        public float sprintSpCost;
+        
+        /// <summary>回避のスタミナ消費量</summary>
+        public float dodgeSpCost;
+        
+        /// <summary>パリィのスタミナ消費量</summary>
+        public float parrySpCost;
+        
+        /// <summary>ガードのスタミナ消費量</summary>
+        public float guardSpCost;
+
+        /// <summary>特殊攻撃のスタミナ消費量</summary>
+        public float attackSpecialSpCost;
+
+        /// <summary>EX攻撃のEP消費量</summary>
+        public float attackExtraEpCost;
         
         /// <summary>移動状態の移動速度</summary>
         [Range(0f, 100f)] 
@@ -17,10 +44,6 @@ namespace SO.Player
         [Range(0f, 100f)] 
         public float sprintSpeed = 12.5f;
 
-        /// <summary>回避時に前方へ加える力</summary>
-        [Range(0f, 100f)] 
-        public float dodgePower = 10f;
-
         /// <summary>特殊攻撃時に前方へ加える力</summary>
         [Range(0f, 100f)] 
         public float attackSpecial4Power = 10f;
@@ -28,8 +51,5 @@ namespace SO.Player
         /// <summary>EX攻撃時に移動方向へ加える力</summary>
         [Range(0f, 100f)] 
         public float attackExtraSpeed = 10f;
-
-        /// <summary>攻撃時のエイム補正速度</summary>
-        public float attackAimAssistSpeed;
     }
 }
