2dea7330b0a417cc1e328eeec7c30458967d6eaf
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-06-15 08:05:00 +0900

    [add] Cyberの攻撃ハンドラーを追加

---
+++ Assets/Scripts/Player/Handler/CyberAttackHandler.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Player.Interface;
+
+namespace Player.Handler
+{
+    /// <summary>
+    /// Cyber（プレイヤー）の攻撃を制御するクラス
+    /// </summary>
+    public class CyberAttackHandler : MonoBehaviour, IPlayerAttackHandler
+    {
+        
+    }
+}
