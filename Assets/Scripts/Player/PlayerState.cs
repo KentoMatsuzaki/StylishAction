f38c36c5f8448660f651850623e735b054d9404b
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-06-21 10:27:51 +0900

    [add] プレイヤーの状態のアクションに物理演算を追加

--- Assets/Scripts/Player/PlayerState.cs
+++ Assets/Scripts/Player/PlayerState.cs
@@ -8,9 +8,10 @@ namespace Player
     /// </summary>
     public class PlayerState
     {
-        public Action OnEnter;  // 状態の開始時に呼ばれるアクション
-        public Action OnUpdate; // 状態の更新時に呼ばれるアクション
-        public Action OnExit;   // 状態の終了時に呼ばれるアクション
+        public Action OnEnter;       // 状態の開始時に呼ばれるアクション
+        public Action OnUpdate;      // 状態の更新時に呼ばれるアクション（Update）
+        public Action OnExit;        // 状態の終了時に呼ばれるアクション
+        public Action OnFixedUpdate; // 状態の更新時に呼ばれるアクション（FixedUpdate）
         
         public readonly InGameEnums.PlayerStateType StateType; // 状態の種類
 
@@ -20,11 +21,12 @@ namespace Player
         }
         
         /// <summary>各アクションを設定する</summary>
-        public void SetAction(Action onEnter = null, Action onUpdate = null, Action onExit = null)
+        public void SetAction(Action onEnter = null, Action onUpdate = null, Action onExit = null, Action onFixedUpdate = null)
         {
             OnEnter = onEnter;
             OnUpdate = onUpdate;
             OnExit = onExit;
+            OnFixedUpdate = onFixedUpdate;
         }
     }
 }
