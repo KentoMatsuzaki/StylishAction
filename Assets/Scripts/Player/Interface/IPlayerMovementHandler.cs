35e090980fb5f90771e3d2f4ae3f680fc8e08deb
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-06-21 10:34:13 +0900

    [add] プレイヤーの移動に関するインターフェースのメソッドを全て実装

--- Assets/Scripts/Player/Interface/IPlayerMovementHandler.cs
+++ Assets/Scripts/Player/Interface/IPlayerMovementHandler.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 namespace Player.Interface
 {
     /// <summary>
-    /// プレイヤーの移動を制御するインターフェース
+    /// プレイヤーの移動制御に関するインターフェース
     /// </summary>
     public interface IPlayerMovementHandler
     {
+        /// <summary>移動方向</summary>
+        Vector3 MoveDirection { get; } 
+        
         /// <summary>入力方向に基づいて移動方向を設定する</summary>
         /// <param name="inputDirection">入力方向</param>
         void SetMoveDirection(Vector2 inputDirection);
+        
+        /// <summary>移動方向を初期化する</summary>
+        void ResetMoveDirection();
+        
+        /// <summary>正面方向に力を加えて移動させる</summary>
+        /// <param name="moveForce">加える力の大きさ</param>
+        void MoveForward(float moveForce);
+
+        /// <summary>移動の入力値が存在するかどうか</summary>
+        bool IsMoving();
+
+        /// <summary>ルートモーションによる移動を物理演算を用いて再現する</summary>
+        /// <param name="deltaPosition">アニメーションによる移動の変化量</param>
+        void ApplyRootMotion(Vector3 deltaPosition);
         
         /// <summary>カメラの位置を基準に入力方向へ回転させる</summary>
         /// <param name="cameraTransform">カメラの位置</param>
