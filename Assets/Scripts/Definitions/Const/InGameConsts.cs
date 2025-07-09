6bea6a53b523001bcd74870e1848f14fae4ee6c4
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-09 12:03:51 +0900

    [add] プレイヤーステータスに関する定数を追加

--- Assets/Scripts/Definitions/Const/InGameConsts.cs
+++ Assets/Scripts/Definitions/Const/InGameConsts.cs
@@ -1,5 +1,3 @@
-using JetBrains.Annotations;
-
 namespace Definitions.Const
 {
     /// <summary>
@@ -18,15 +16,15 @@ namespace Definitions.Const
         public const string PlayerRollAnimState = "Roll";         // 回避
         public const string PlayerParryAnimState = "Parry";       // パリィ
         public const string PlayerGuardAnimState = "Guard";       // 防御
+        public const string PlayerGuardHitAnimState = "GuardHit"; // 防御時の被弾
         public const string PlayerAttackNAnimState1 = "Atk_N_01"; // 通常攻撃（1段目）
-        public const string PlayerAttackNAnimState2 = "Atk_N_02"; // 通常攻撃（1段目）
-        public const string PlayerAttackNAnimState3 = "Atk_N_03"; // 通常攻撃（1段目）
-        public const string PlayerAttackNAnimState4 = "Atk_N_04"; // 通常攻撃（1段目）
-        public const string PlayerAttackNAnimState5 = "Atk_N_05"; // 通常攻撃（1段目）
+        public const string PlayerAttackNAnimState2 = "Atk_N_02"; // 通常攻撃（2段目）
+        public const string PlayerAttackNAnimState3 = "Atk_N_03"; // 通常攻撃（3段目）
+        public const string PlayerAttackNAnimState4 = "Atk_N_04"; // 通常攻撃（4段目）
+        public const string PlayerAttackNAnimState5 = "Atk_N_05"; // 通常攻撃（5段目）
         public const string PlayerAttackSAnimState1 = "Atk_S_01"; // 特殊攻撃（1段目）
         public const string PlayerAttackSAnimState2 = "Atk_S_02"; // 特殊攻撃（2段目）
         public const string PlayerAttackSAnimState3 = "Atk_S_03"; // 特殊攻撃（3段目）
-        public const string PlayerAttackSAnimState4 = "Atk_S_04"; // 特殊攻撃（4段目）
         public const string PlayerAttackEAnimState = "Atk_E";     // EX攻撃
         public const string PlayerDamageAnimState = "Damage";     // 被弾
         public const string PlayerDeadAnimState = "Dead";         // 死亡
@@ -37,6 +35,9 @@ namespace Definitions.Const
         
         public const string DeathIdleAnimState = "Idle";
         public const string DeathMoveAnimState = "Move";
+        public const string DeathParriedAnimState = "Parried";
+        public const string DeathRecoveryAnimState = "Recovery";
+        public const string DeathDieAnimState = "Die";
         public const string DeathScytheAnimState = "Scythe";
         public const string DeathMeteorAnimState = "Meteor";
         public const string DeathSeraphAnimState = "Seraph";
@@ -45,5 +46,23 @@ namespace Definitions.Const
         public const string DeathPhotonAnimState = "Photon";
         public const string DeathExplosionAnimState = "Explosion";
         public const string DeathWaterfallAnimState = "Waterfall";
+        
+        //-------------------------------------------------------------------------------
+        // GameObjectのタグ名
+        //-------------------------------------------------------------------------------
+        
+        public const string PlayerGameObjectTag = "Player";
+        public const string EnemyGameObjectTag = "Enemy";
+        
+        //-------------------------------------------------------------------------------
+        // プレイヤーのステータス
+        //-------------------------------------------------------------------------------
+
+        public const float PlayerSpRegenerateRate = 0.05f;
+        public const float PlayerDashSpCost = 0.001f;
+        public const float PlayerRollSpCost = 0.125f;
+        public const float PlayerParrySpCost = 0.25f;
+        public const float PlayerGuardSpCost = 0.005f;
+        public const float PlayerAtkSSpCost = 0.25f;
     }
 }
