0047bf89cc060d8df7df078d962fbc21ebd24d67
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-09 12:10:29 +0900

    [add] 敵の攻撃に関するインターフェースを実装

--- Assets/Scripts/Enemy/Interface/IEnemyAnimationHandler.cs
+++ Assets/Scripts/Enemy/Interface/IEnemyAnimationHandler.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Definitions.Enum;
 
 namespace Enemy.Interface
@@ -6,6 +7,9 @@ namespace Enemy.Interface
     /// <summary>敵のアニメーション制御に関するインターフェース</summary>
     public interface IEnemyAnimationHandler
     {
+        /// <summary>移動アニメーションを再生中かどうか</summary>
+        bool IsPlayingMoveAnimation { get; set; }
+        
         /// <summary>指定した種類の攻撃アニメーションを再生する</summary>
         void PlayAttackAnimation(InGameEnums.EnemyAttackType type);
         
@@ -14,5 +18,17 @@ namespace Enemy.Interface
         
         /// <summary>移動アニメーションを再生する</summary>
         void PlayMoveAnimation();
+        
+        /// <summary>被パリィアニメーションを再生する</summary>
+        void PlayParriedAnimation();
+        
+        /// <summary>復帰アニメーションを再生する</summary>
+        void PlayRecoveryAnimation();
+
+        /// <summary>死亡アニメーションを再生する</summary>
+        void PlayDieAnimation();
+
+        /// <summary>アニメーションの再生完了を待機する</summary>
+        UniTask WaitUntilAnimationComplete();
     }
 }
