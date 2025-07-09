040d174c1aee33713669ccdee880f7a0ab37b32b
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-09 12:30:29 +0900

    [add] プレイヤーの攻撃判定を制御するクラスを追加・実装

---
+++ Assets/Scripts/Player/Attack/PlayerAttacker.cs
@@ -0,0 +1,74 @@
+using Common;
+using Definitions.Const;
+using Definitions.Data;
+using Definitions.Enum;
+using Enemy.AI;
+using Managers;
+using Player.Controller;
+using UnityEngine;
+
+namespace Player.Attack
+{
+    /// <summary>
+    /// プレイヤーの攻撃判定を制御する派生クラス
+    /// AttackerBaseを継承し、プレイヤー固有の処理を実装する
+    /// </summary>
+    public class PlayerAttacker : AttackerBase
+    {
+        [SerializeField] private PlayerAttackStats attackStats; // 攻撃のパラメーター
+        private PlayerControllerBase _player;
+        
+        //-------------------------------------------------------------------------------
+        // 初期化に関する処理
+        //-------------------------------------------------------------------------------
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Collider.enabled = true;
+            _player = GetComponentInParent<PlayerControllerBase>();
+        }
+        
+        //-------------------------------------------------------------------------------
+        // 攻撃判定に関する処理
+        //-------------------------------------------------------------------------------
+
+        protected override void OnTriggerEnter(Collider other)
+        {
+            // 敵のゲームオブジェクトと接触した場合
+            if (other.CompareTag(InGameConsts.EnemyGameObjectTag))
+            {
+                var enemy = other.GetComponent<EnemyAIBase>(); // 敵AIの制御クラス
+                var hitPos = other.ClosestPoint(transform.position); // 攻撃が命中したワールド座標
+                var damage = Random.Range(attackStats.damage * 0.75f, attackStats.damage);
+                enemy.OnHit(damage, hitPos); // 攻撃命中時の処理を呼ぶ
+                ShowDamageUI(hitPos, damage); // ダメージUIを表示する
+
+                if (attackStats.attackType == InGameEnums.PlayerAttackType.Normal)
+                {
+                    _player.IncreaseEp();
+                    Debug.Log($"{_player.PlayerEp}");
+                }
+            }
+        }
+
+        private void ShowDamageUI(Vector3 hitPos, float damage)
+        {
+            switch (attackStats.attackType)
+            {
+                case InGameEnums.PlayerAttackType.Normal:
+                    UIManager.Instance.ShowDamageUI(hitPos, damage, Color.yellow, Color.white);
+                    break;
+                
+                case InGameEnums.PlayerAttackType.Special:
+                    UIManager.Instance.ShowDamageUI(hitPos, damage, Color.cyan, Color.white);
+                    break;
+                
+                case InGameEnums.PlayerAttackType.Extra:
+                    UIManager.Instance.ShowDamageUI(hitPos, damage,
+                        new Color(0.4f, 0.2f, 1.0f, 1.0f), new Color(0.85f, 0.8f, 1.0f, 1.0f));
+                    break;
+            }
+        }
+    }
+}
