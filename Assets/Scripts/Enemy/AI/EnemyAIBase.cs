4316a300046fd8139ec954c2f30ea5238a99748a
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-09 12:05:24 +0900

    [add] 敵AIのベースクラスに被弾処理を追加

--- Assets/Scripts/Enemy/AI/EnemyAIBase.cs
+++ Assets/Scripts/Enemy/AI/EnemyAIBase.cs
@@ -15,19 +15,20 @@ namespace Enemy.AI
     /// </summary>
     public abstract class EnemyAIBase : MonoBehaviour
     {
-        protected IEnemyAttackHandler AttackHandler; // 攻撃ハンドラー
-        protected IEnemyMovementHandler MovementHandler; // 移動ハンドラー
-        protected IEnemyAnimationHandler AnimationHandler; // アニメーション
+        protected IEnemyAttackHandler AttackHandler;       // 攻撃ハンドラー
+        protected IEnemyMovementHandler MovementHandler;   // 移動ハンドラー
+        protected IEnemyParticleHandler ParticleHandler;   // パーティクルハンドラー
+        protected IEnemyAnimationHandler AnimationHandler; // アニメーションハンドラー
 
-        protected ReactiveProperty<float> CurrentHp = new(); // 現在のHP
-        public IReadOnlyReactiveProperty<float> EnemyHp => CurrentHp;
+        private readonly ReactiveProperty<float> _currentHp = new(); // 現在のHP
+        public IReadOnlyReactiveProperty<float> EnemyHp => _currentHp;
         
         public float MaxHp { get; private set; } // 最大HP
         
         protected EnemyBaseStats BaseStats; // 基本パラメーター
 
-        protected AsyncSelectorNode RootNode = new(); // 最上位ノード
-        private CancellationTokenSource _cts; // キャンセルトークン
+        protected readonly AsyncSelectorNode RootNode = new(); // 最上位ノード
+        protected CancellationTokenSource Cts; // キャンセルトークン
         
         //-------------------------------------------------------------------------------
         // 初期化に関する処理
@@ -47,13 +48,14 @@ namespace Enemy.AI
         {
             AttackHandler = GetComponent<IEnemyAttackHandler>();
             MovementHandler = GetComponent<IEnemyMovementHandler>();
+            ParticleHandler = GetComponent<IEnemyParticleHandler>();
             AnimationHandler = GetComponent<IEnemyAnimationHandler>();
         }
 
         private void InitializeStats(EnemyBaseStats baseStats)
         {
             BaseStats = baseStats;
-            CurrentHp.Value = BaseStats.maxHp;
+            _currentHp.Value = BaseStats.maxHp;
             MaxHp = BaseStats.maxHp;
         }
         
@@ -69,10 +71,10 @@ namespace Enemy.AI
         {
             try
             {
-                while (!_cts.IsCancellationRequested)
+                while (!Cts.IsCancellationRequested)
                 {
-                    await RootNode.ExecuteAsync(_cts.Token);
-                    await UniTask.Yield(PlayerLoopTiming.Update, _cts.Token);
+                    await RootNode.ExecuteAsync(Cts.Token);
+                    await UniTask.Yield(PlayerLoopTiming.Update, Cts.Token);
                 }
             }
             catch (OperationCanceledException)
@@ -82,12 +84,45 @@ namespace Enemy.AI
         }
 
         /// <summary>BehaviourTreeの実行を開始する</summary>
-        private void StartBehaviourTree()
+        protected void StartBehaviourTree()
         {
-            _cts?.Cancel();
-            _cts = new CancellationTokenSource();
+            Cts?.Cancel();
+            Cts = new CancellationTokenSource();
             AttackHandler.SetAttackStats();
             ExecuteBehaviourTree().Forget();
         }
+        
+        //-------------------------------------------------------------------------------
+        // プレイヤーの攻撃が命中した際の処理
+        //-------------------------------------------------------------------------------
+
+        /// <summary>プレイヤーの攻撃を受けた際に呼ばれる処理</summary>
+        /// <param name="damage">受けた攻撃のダメージ量</param>
+        /// <param name="hitPosition">攻撃がヒットしたワールド座標</param>
+        public abstract void OnHit(float damage, Vector3 hitPosition);
+
+        /// <summary>ダメージの数値だけHPを減少させ、適切なメソッドを呼び出す</summary>
+        protected void ApplyDamage(float damage)
+        {
+            _currentHp.Value = Mathf.Max(0, _currentHp.Value - damage);
+            
+            if (_currentHp.Value > 0)
+            {
+                OnDamage();
+            }
+            else
+            {
+                OnDie();
+            }
+        }
+
+        /// <summary>被弾時の処理</summary>
+        protected abstract void OnDamage();
+
+        /// <summary>死亡時の処理</summary>
+        protected abstract void OnDie();
+
+        /// <summary>パリィされた時の処理</summary>
+        public abstract void OnParried();
     }
 }
