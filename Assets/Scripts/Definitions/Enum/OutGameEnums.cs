709f3516d798b9c896d1ac1c855516e6d016370a
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-09 12:06:27 +0900

    [add] サウンド・UIに関する列挙型を追加

--- Assets/Scripts/Definitions/Enum/OutGameEnums.cs
+++ Assets/Scripts/Definitions/Enum/OutGameEnums.cs
@@ -5,6 +5,41 @@ namespace Definitions.Enum
     /// </summary>
     public class OutGameEnums
     {
+        //-------------------------------------------------------------------------------
+        // サウンドに関する列挙型
+        //-------------------------------------------------------------------------------
+
+        public enum SoundType
+        {
+            BGM,
+            Button,
+            Clear,
+            GameOver,
+            AttackN,
+            AttackS1,
+            AttackS2,
+            Parry,
+            GuardHit,
+            PlayerHit,
+            EnemyHit,
+            Rolling,
+            Scythe,
+            Meteor,
+            Seraph,
+            Vortex,
+            Photon,
+            Explosion,
+            Waterfall
+        }
         
+        //-------------------------------------------------------------------------------
+        // UIに関する列挙型
+        //-------------------------------------------------------------------------------
+
+        public enum ButtonType
+        {
+            Start,
+            Quit,
+        }
     }
 }
