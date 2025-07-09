9309758e1b9ee33552e6b334224330815bec4219
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-09 12:10:43 +0900

    [add] 敵の移動に関するインターフェースを実装

--- Assets/Scripts/Enemy/Interface/IEnemyMovementHandler.cs
+++ Assets/Scripts/Enemy/Interface/IEnemyMovementHandler.cs
@@ -3,9 +3,12 @@ namespace Enemy.Interface
     /// <summary>敵の移動制御に関するインターフェース</summary>
     public interface IEnemyMovementHandler
     {
-        /// <summary>プレイヤーの方向に回転させる</summary>
+        /// <summary>プレイヤーの方向に緩やかに回転させる</summary>
         /// <param name="rotateSpeed">回転速度</param>
         void RotateTowardsPlayer(float rotateSpeed);
+
+        /// <summary>プレイヤーの方向に即座に回転させる</summary>
+        void RotateTowardsPlayerInstantly();
         
         /// <summary>プレイヤーの方向に力を加えて移動させる</summary>
         /// <param name="moveForce">移動時に加える力の大きさ</param>
@@ -14,5 +17,9 @@ namespace Enemy.Interface
         /// <summary>プレイヤーの逆方向に力を加えて移動させる</summary>
         /// /// <param name="moveForce">移動時に加える力の大きさ</param>
         void MoveAwayFromPlayer(float moveForce);
+        
+        /// <summary>プレイヤーの逆方向に力を加えてノックバックを再現する</summary>
+        /// <param name="knockBackForce">ノックバック時に加える力の大きさ</param>
+        void ApplyKnockBack(float knockBackForce);
     }
 }
