aa7dde170636c0da748386eb621c7ebd460c54a6
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-26 00:47:49 +0900

    [del] ダッシュ時に加える力の大きさ [add] ロックオン時・非ロックオン時の移動時に加える力の大きさ

--- Assets/Scripts/Definitions/Data/PlayerBaseStats.cs
+++ Assets/Scripts/Definitions/Data/PlayerBaseStats.cs
@@ -11,8 +11,8 @@ namespace Definitions.Data
         public float maxHp; // 最大HP
         public float maxSp; // 最大SP
         public float maxEp; // 最大EP
-        public float moveForce; // 移動時に加える力の大きさ
-        public float dashForce; // ダッシュ時に加える力の大きさ
+        public float moveForce; // 移動時に加える力の大きさ（非ロックオン時）
+        public float strafeForce; // 移動時に加える力の大きさ（ロックオン時）
         public float atkEForce; // EX攻撃時に加える力の大きさ
     }
 }
