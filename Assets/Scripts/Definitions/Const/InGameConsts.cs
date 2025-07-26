37a9b7d97d63bb11666d5445ab0c5bb4ba0fadf5
KentoMatsuzaki <vantanstudent@outlook.jp>
2025-07-26 15:17:15 +0900

    [add] 防御のクールタイムを表す定数を追加

--- Assets/Scripts/Definitions/Const/InGameConsts.cs
+++ Assets/Scripts/Definitions/Const/InGameConsts.cs
@@ -68,5 +68,6 @@ namespace Definitions.Const
 
         public const float PlayerRollCoolDown = 5.0f;
         public const float PlayerParryCoolDown = 5.0f;
+        public const float PlayerGuardCoolDown = 5.0f;
     }
 }
